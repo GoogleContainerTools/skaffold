65f90895cb2073df010ea049786409972df5de92
Matt Rickard <mrick@google.com>
2018-01-28 15:21:18 -0800

    Add sha256 tagger implementation

---
+++ pkg/skaffold/build/tag/sha256.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tag
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ChecksumTagger tags an image by the sha256 of the image tarball
+type ChecksumTagger struct {
+	ImageName string
+	Checksum  string
+}
+
+// GenerateTag tags an image with the supplied image name and the sha256 checksum of the image
+func (c *ChecksumTagger) GenerateTag() (string, error) {
+	return fmt.Sprintf("%s:%s", c.ImageName, c.Checksum), nil
+}
+
+// NewChecksumTaggerFromDigest returns a tagger instance by splitting the docker digest
+// into identifier and checksum
+func NewChecksumTaggerFromDigest(digest, imageName string) (*ChecksumTagger, error) {
+	digestSplit := strings.Split(digest, ":")
+	if len(digestSplit) != 2 {
+		return nil, fmt.Errorf("Digest wrong format: %s, expected sha256:<checksum>", digestSplit)
+	}
+	checksum := digestSplit[1]
+	return &ChecksumTagger{
+		ImageName: imageName,
+		Checksum:  checksum,
+	}, nil
+}
---
+++ pkg/skaffold/build/tag/sha256_test.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tag
+
+import (
+	"testing"
+
+	testutil "github.com/GoogleCloudPlatform/skaffold/test"
+)
+
+func TestNewChecksumTaggerFromDigest(t *testing.T) {
+	var tests = []struct {
+		description string
+		digest      string
+		image       string
+
+		shouldErr bool
+	}{
+		{
+			description: "no error",
+			digest:      "sha256:12345abcde",
+			image:       "test",
+		},
+		{
+			description: "wrong digest format",
+			digest:      "wrong:digest:format",
+			image:       "test",
+			shouldErr:   true,
+		},
+		{
+			description: "wrong digest format no colon",
+			digest:      "sha256",
+			image:       "test",
+			shouldErr:   true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			_, err := NewChecksumTaggerFromDigest(test.digest, test.image)
+			testutil.CheckError(t, test.shouldErr, err)
+		})
+	}
+}
+
+func TestGenerateTag(t *testing.T) {
+	tagger := &ChecksumTagger{ImageName: "test", Checksum: "1234"}
+	if _, err := tagger.GenerateTag(); err != nil {
+		t.Errorf("Error generating tag: %s", err)
+	}
+}
