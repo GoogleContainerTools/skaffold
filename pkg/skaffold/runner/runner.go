651c078855e430351842e040896a72c2f9108a3e
Matt Rickard <mrick@google.com>
2018-01-28 15:31:01 -0800

    Add runner package and tests

---
+++ pkg/skaffold/runner/runner.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package runner
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/GoogleCloudPlatform/skaffold/pkg/skaffold/build"
+	"github.com/GoogleCloudPlatform/skaffold/pkg/skaffold/config"
+	"github.com/pkg/errors"
+)
+
+// SkaffoldRunner is responsible for running the skaffold build and deploy pipeline.
+type SkaffoldRunner struct {
+	build.Builder
+	*config.SkaffoldConfig
+
+	out io.Writer
+}
+
+// NewForConfig returns a new SkaffoldRunner for a SkaffoldConfig
+func NewForConfig(out io.Writer, cfg *config.SkaffoldConfig) (*SkaffoldRunner, error) {
+	builder, err := getBuilder(&cfg.Build)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing skaffold config")
+	}
+	return &SkaffoldRunner{
+		SkaffoldConfig: cfg,
+		Builder:        builder,
+		out:            out,
+	}, nil
+}
+
+func getBuilder(cfg *config.BuildConfig) (build.Builder, error) {
+	if cfg != nil && cfg.LocalBuild != nil {
+		return build.NewLocalBuilder(cfg)
+	}
+	return nil, fmt.Errorf("Unknown builder for config %+v", cfg)
+}
+
+// Run runs the skaffold build and deploy pipeline.
+func (r *SkaffoldRunner) Run() error {
+	_, err := r.Builder.Run(r.out)
+	if err != nil {
+		return errors.Wrap(err, "build step")
+	}
+	// Deploy
+	return nil
+}
---
+++ pkg/skaffold/runner/runner_test.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package runner
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/skaffold/pkg/skaffold/build"
+	"github.com/GoogleCloudPlatform/skaffold/pkg/skaffold/config"
+	testutil "github.com/GoogleCloudPlatform/skaffold/test"
+)
+
+type TestBuilder struct {
+	res *build.BuildResult
+	err error
+}
+
+func (t *TestBuilder) Run(io.Writer) (*build.BuildResult, error) {
+	return t.res, t.err
+}
+
+func TestNewForConfig(t *testing.T) {
+	var tests = []struct {
+		description string
+		config      *config.SkaffoldConfig
+		shouldErr   bool
+		expected    interface{}
+	}{
+		{
+			description: "local builder config",
+			config: &config.SkaffoldConfig{
+				Build: config.BuildConfig{
+					BuildType: config.BuildType{
+						LocalBuild: &config.LocalBuild{},
+					},
+				},
+			},
+			expected: &build.LocalBuilder{},
+		},
+		{
+			description: "unknown builder",
+			config: &config.SkaffoldConfig{
+				Build: config.BuildConfig{},
+			},
+			shouldErr: true,
+			expected:  &build.LocalBuilder{},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			cfg, err := NewForConfig(&bytes.Buffer{}, test.config)
+			testutil.CheckError(t, test.shouldErr, err)
+			if cfg != nil {
+				testutil.CheckErrorAndTypeEquality(t, test.shouldErr, err, test.expected, cfg.Builder)
+			}
+		})
+	}
+}
+
+func TestRun(t *testing.T) {
+	var tests = []struct {
+		description string
+		runner      *SkaffoldRunner
+		shouldErr   bool
+	}{
+		{
+			description: "run no error",
+			runner: &SkaffoldRunner{
+				Builder: &TestBuilder{
+					res: &build.BuildResult{},
+					err: nil,
+				},
+			},
+		},
+		{
+			description: "run build error",
+			runner: &SkaffoldRunner{
+				Builder: &TestBuilder{
+					err: fmt.Errorf(""),
+				},
+			},
+			shouldErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			err := test.runner.Run()
+			testutil.CheckError(t, test.shouldErr, err)
+		})
+	}
+}
