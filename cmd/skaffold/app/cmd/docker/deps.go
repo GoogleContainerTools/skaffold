4df2b2b66a579812bca6ed580ba89361ecf7955e
Matt Rickard <mrick@google.com>
2018-02-02 09:59:28 -0800

    Add docker deps command

---
+++ cmd/skaffold/app/cmd/docker/deps.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package docker
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/skaffold/pkg/skaffold/docker"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	filename, context string
+)
+
+func NewCmdDeps(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deps",
+		Short: "Returns a list of dependencies for the input dockerfile",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := runDeps(out, filename, context); err != nil {
+				logrus.Fatalf("docker deps: %s", err)
+			}
+		},
+	}
+	cmd.Flags().StringVarP(&filename, "filename", "f", "Dockerfile", "Dockerfile path")
+	cmd.Flags().StringVarP(&context, "context", "c", ".", "Dockerfile context path")
+
+	return cmd
+}
+
+func runDeps(out io.Writer, filename, context string) error {
+	f, err := os.Open(filepath.Join(context, filename))
+	if err != nil {
+		return errors.Wrap(err, "opening dockerfile")
+	}
+	deps, err := docker.GetDockerfileDependencies(context, f)
+	if err != nil {
+		return errors.Wrap(err, "getting dockerfile dependencies")
+	}
+	outStr := fmt.Sprintf("%s\n", strings.Join(deps, " "))
+	if _, err := io.WriteString(out, outStr); err != nil {
+		return errors.Wrap(err, "writing deps to out writer")
+	}
+	return nil
+}
