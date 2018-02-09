5fd0e043e51b0aff8e5c5ecb3018b8d49747c794
Matt Rickard <mrick@google.com>
2018-02-08 22:22:48 -0800

    Add travis integration test config

--- hack/travis/gcloud.sh
+++ hack/travis/gcloud.sh
@@ -2,7 +2,9 @@ set -e
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export PROJECT_ID=k8s-skaffold
-export CLUSTER=
+export CLUSTER=integration-tests
+export ZONE=us-central1-a
+export GOOGLE_APPLICATION_CREDENTIALS=k8s-skaffold-c0bfe91b623b.json
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then 
     rm -rf $HOME/google-cloud-sdk
@@ -13,6 +15,5 @@ source /home/travis/google-cloud-sdk/path.bash.inc
 
 gcloud components update kubectl
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
-gcloud config set project ${PROJECT_ID}
-gcloud container clusters get-credentials ${CLUSTER}
+gcloud container clusters get-credentials ${CLUSTER} --zone ${ZONE} --project ${PROJECT_ID}
 gcloud beta auth configure-docker
