87a1e4d076b49d0a19b2a12da7d9739c8cd9df2b
Matt Rickard <mrick@google.com>
2018-02-10 13:56:56 -0800

    kube: add current context getter

---
+++ pkg/skaffold/kubernetes/context.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubernetes
+
+import (
+	"github.com/pkg/errors"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func CurrentContext() (string, error) {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
+	cfg, err := kubeConfig.RawConfig()
+	if err != nil {
+		return "", errors.Wrap(err, "loading kubeconfig")
+	}
+	return cfg.CurrentContext, nil
+}
