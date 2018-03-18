722be7df6c5e66918487e1284eb21537a287e502
David Gageot <david@gageot.net>
2018-03-18 10:20:30 +0100

    Remember deployed tags
    Fixes #221
    
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/runner/runner.go
+++ pkg/skaffold/runner/runner.go
@@ -44,6 +44,7 @@ type SkaffoldRunner struct {
 	opts       *config.SkaffoldOptions
 	config     *config.SkaffoldConfig
 	kubeclient clientgo.Interface
+	builds     []build.Build
 }
 
 var kubernetesClient = kubernetes.GetClientset
@@ -128,7 +129,7 @@ func (r *SkaffoldRunner) Run() error {
 		return r.dev(r.config.Build.Artifacts)
 	}
 
-	if _, _, err := r.run(r.config.Build.Artifacts); err != nil {
+	if _, _, err := r.buildAndDeploy(r.config.Build.Artifacts); err != nil {
 		return errors.Wrap(err, "run")
 	}
 	return nil
@@ -143,10 +144,10 @@ func (r *SkaffoldRunner) dev(artifacts []*config.Artifact) error {
 	logger := kubernetes.NewLogAggregator(r.opts.Output)
 
 	onChange := func(artifacts []*config.Artifact) {
+		logger.SetCreationTime(time.Now())
 		logger.Mute()
 
-		logger.SetCreationTime(time.Now())
-		bRes, _, err := r.run(artifacts)
+		bRes, _, err := r.buildAndDeploy(artifacts)
 		if err != nil {
 			// In dev mode, we only warn on pipeline errors
 			logrus.Warnf("run: %s", err)
@@ -167,24 +168,69 @@ func (r *SkaffoldRunner) dev(artifacts []*config.Artifact) error {
 	return nil
 }
 
-func (r *SkaffoldRunner) run(artifacts []*config.Artifact) (*build.BuildResult, *deploy.Result, error) {
-	start := time.Now()
-	fmt.Fprintln(r.opts.Output, "Starting build...")
-	bRes, err := r.Builder.Build(r.opts.Output, r.Tagger, artifacts)
+func (r *SkaffoldRunner) buildAndDeploy(artifacts []*config.Artifact) (*build.BuildResult, *deploy.Result, error) {
+	bRes, err := r.build(artifacts)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "build step")
 	}
+
+	r.builds = mergeWithPreviousBuilds(bRes.Builds, r.builds)
+
+	dRes, err := r.deploy(&build.BuildResult{
+		Builds: r.builds,
+	})
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "build step")
+	}
+
+	return bRes, dRes, nil
+}
+
+func (r *SkaffoldRunner) build(artifacts []*config.Artifact) (*build.BuildResult, error) {
+	start := time.Now()
+	fmt.Fprintln(r.opts.Output, "Starting build...")
+
+	bRes, err := r.Builder.Build(r.opts.Output, r.Tagger, artifacts)
+	if err != nil {
+		return nil, errors.Wrap(err, "build step")
+	}
+
 	fmt.Fprintln(r.opts.Output, "Build complete in", time.Now().Sub(start))
 
-	start = time.Now()
+	return bRes, nil
+}
+
+func (r *SkaffoldRunner) deploy(bRes *build.BuildResult) (*deploy.Result, error) {
+	start := time.Now()
 	fmt.Fprintln(r.opts.Output, "Starting deploy...")
-	if _, err := r.Deployer.Deploy(r.opts.Output, bRes); err != nil {
-		return nil, nil, errors.Wrap(err, "deploy step")
+
+	dRes, err := r.Deployer.Deploy(r.opts.Output, bRes)
+	if err != nil {
+		return nil, errors.Wrap(err, "deploy step")
 	}
 	if r.opts.Notification {
 		fmt.Fprint(r.opts.Output, constants.TerminalBell)
 	}
+
 	fmt.Fprintln(r.opts.Output, "Deploy complete in", time.Now().Sub(start))
 
-	return bRes, nil, nil
+	return dRes, nil
+}
+
+func mergeWithPreviousBuilds(builds, previous []build.Build) []build.Build {
+	updatedBuilds := map[string]bool{}
+	for _, build := range builds {
+		updatedBuilds[build.ImageName] = true
+	}
+
+	var merged []build.Build
+	merged = append(merged, builds...)
+
+	for _, b := range previous {
+		if !updatedBuilds[b.ImageName] {
+			merged = append(merged, b)
+		}
+	}
+
+	return merged
 }
