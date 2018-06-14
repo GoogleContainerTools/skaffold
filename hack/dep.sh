f97ea7aa9424530958aa2af67b875777a282b98c
David Gageot <david@gageot.net>
2018-06-14 10:39:17 -0700

    Fix hack/dep.sh on travisCI
    Fixes #670

--- hack/dep.sh
+++ hack/dep.sh
@@ -30,7 +30,7 @@ install_dep() {
 }
 
 if [ -z "$VALIDATE_UPSTREAM" ]; then
-	VALIDATE_REPO='git@github.com:GoogleContainerTools/skaffold.git'
+	VALIDATE_REPO='https://github.com/GoogleContainerTools/skaffold.git'
 	VALIDATE_BRANCH='master'
 
 	VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
