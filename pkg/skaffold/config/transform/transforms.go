f8f235a6a1dcad9fbf7dbccae72fa9695dd73515
stefanthorpe <stefan.thorpe@gmail.com>
2018-07-06 13:35:21 +0700

    Removed duplicates

--- pkg/skaffold/config/transform/transforms.go
+++ pkg/skaffold/config/transform/transforms.go
@@ -33,17 +33,6 @@ func ToV1alpha3(vc util.VersionedConfig) (util.VersionedConfig, error) {
 	}
 	oldConfig := vc.(*v1alpha2.SkaffoldConfig)
 
-	var tagPolicy v1alpha3.TagPolicy
-	if oldConfig.Build.TagPolicy == constants.TagStrategySha256 {
-		tagPolicy = v1alpha3.TagPolicy{
-			ShaTagger: &v1alpha3.ShaTagger{},
-		}
-	} else if oldConfig.Build.TagPolicy == constants.TagStrategyGitCommit {
-		tagPolicy = v1alpha3.TagPolicy{
-			GitTagger: &v1alpha3.GitTagger{},
-		}
-	}
-
 	var newHelmDeploy *v1alpha3.HelmDeploy
 	if oldConfig.Deploy.DeployType.HelmDeploy != nil {
 		newReleases := make([]v1alpha3.HelmRelease, 0)
@@ -55,7 +44,7 @@ func ToV1alpha3(vc util.VersionedConfig) (util.VersionedConfig, error) {
 				Namespace:         release.Namespace,
 				Version:           release.Version,
 				SetValues:         release.SetValues,
-				SetValueTemplates: release.SetValuesTemplates,
+				SetValueTemplates: release.SetValueTemplates,
 				Wait:              release.Wait,
 				Overrides:         release.Overrides,
 				Packaged:          release.Packaged,
@@ -64,96 +53,6 @@ func ToV1alpha3(vc util.VersionedConfig) (util.VersionedConfig, error) {
 		newHelmDeploy = &v1alpha3.HelmDeploy{
 			Releases: newReleases,
 		}
-	}
-	return newConfig, nil
-}
-
-func ToV1Alpha2(vc util.VersionedConfig) (util.VersionedConfig, error) {
-	if vc.GetVersion() != v1alpha1.Version {
-		return nil, fmt.Errorf("Incompatible version: %s", vc.GetVersion())
-	}
-	oldConfig := vc.(*v1alpha1.SkaffoldConfig)
-
-	var tagPolicy v1alpha2.TagPolicy
-	if oldConfig.Build.TagPolicy == constants.TagStrategySha256 {
-		tagPolicy = v1alpha2.TagPolicy{
-			ShaTagger: &v1alpha2.ShaTagger{},
-		}
-	} else if oldConfig.Build.TagPolicy == constants.TagStrategyGitCommit {
-		tagPolicy = v1alpha2.TagPolicy{
-			GitTagger: &v1alpha2.GitTagger{},
-		}
-	}
-
-	var newHelmDeploy *v1alpha2.HelmDeploy
-	if oldConfig.Deploy.DeployType.HelmDeploy != nil {
-		newReleases := make([]v1alpha2.HelmRelease, 0)
-		for _, release := range oldConfig.Deploy.DeployType.HelmDeploy.Releases {
-			newReleases = append(newReleases, v1alpha2.HelmRelease{
-				Name:           release.Name,
-				ChartPath:      release.ChartPath,
-				ValuesFilePath: release.ValuesFilePath,
-				Values:         release.Values,
-				Namespace:      release.Namespace,
-				Version:        release.Version,
-			})
-		}
-		newHelmDeploy = &v1alpha2.HelmDeploy{
-			Releases: newReleases,
-		}
-	}
-	var newKubectlDeploy *v1alpha2.KubectlDeploy
-	if oldConfig.Deploy.DeployType.KubectlDeploy != nil {
-		newManifests := make([]string, 0)
-		logrus.Warn("Ignoring manifest parameters when transforming v1alpha1 config; check kubernetes yaml before running skaffold")
-		for _, manifest := range oldConfig.Deploy.DeployType.KubectlDeploy.Manifests {
-			newManifests = append(newManifests, manifest.Paths...)
-		}
-		newKubectlDeploy = &v1alpha2.KubectlDeploy{
-			Manifests: newManifests,
-		}
-	}
-
-	var newArtifacts = make([]*v1alpha2.Artifact, 0)
-	for _, artifact := range oldConfig.Build.Artifacts {
-		newArtifacts = append(newArtifacts, &v1alpha2.Artifact{
-			ImageName: artifact.ImageName,
-			Workspace: artifact.Workspace,
-			ArtifactType: v1alpha2.ArtifactType{
-				DockerArtifact: &v1alpha2.DockerArtifact{
-					DockerfilePath: artifact.DockerfilePath,
-					BuildArgs:      artifact.BuildArgs,
-				},
-			},
-		})
-	}
-
-	var newBuildType = v1alpha2.BuildType{}
-	if oldConfig.Build.GoogleCloudBuild != nil {
-		newBuildType.GoogleCloudBuild = &v1alpha2.GoogleCloudBuild{
-			ProjectID: oldConfig.Build.GoogleCloudBuild.ProjectID,
-		}
-	}
-	if oldConfig.Build.LocalBuild != nil {
-		newBuildType.LocalBuild = &v1alpha2.LocalBuild{
-			SkipPush: oldConfig.Build.LocalBuild.SkipPush,
-		}
-	}
-
-	newConfig := &v1alpha2.SkaffoldConfig{
-		APIVersion: v1alpha2.Version,
-		Kind:       oldConfig.Kind,
-		Deploy: v1alpha2.DeployConfig{
-			DeployType: v1alpha2.DeployType{
-				HelmDeploy:    newHelmDeploy,
-				KubectlDeploy: newKubectlDeploy,
-			},
-		},
-		Build: v1alpha2.BuildConfig{
-			Artifacts: newArtifacts,
-			BuildType: newBuildType,
-			TagPolicy: tagPolicy,
-		},
 	}
 	return newConfig, nil
 }
