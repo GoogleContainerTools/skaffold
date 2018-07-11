eccf398c525787440319dbfa6e94ead99b662803
David Gageot <david@gageot.net>
2018-07-11 15:39:57 +0200

    Support disk size and machine type for GCB
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/schema/v1alpha2/config.go
+++ pkg/skaffold/schema/v1alpha2/config.go
@@ -85,7 +85,9 @@ type LocalBuild struct {
 // GoogleCloudBuild contains the fields needed to do a remote build on
 // Google Container Builder.
 type GoogleCloudBuild struct {
-	ProjectID string `yaml:"projectId"`
+	ProjectID   string `yaml:"projectId"`
+	DiskSizeGb  int64  `yaml:"diskSizeGb,omitempty"`
+	MachineType string `yaml:"machineType,omitempty"`
 }
 
 // KanikoBuild contains the fields needed to do a on-cluster build using
