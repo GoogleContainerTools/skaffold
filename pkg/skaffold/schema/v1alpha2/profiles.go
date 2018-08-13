6b2020043170119c41ba4df982671b7323c4e772
Nick Kubala <nkubala@google.com>
2018-08-13 16:33:07 -0700

    implement 'oneOf' yamltag which marks struct fields as values, and recursively overlay profiles

--- pkg/skaffold/schema/v1alpha2/profiles.go
+++ pkg/skaffold/schema/v1alpha2/profiles.go
@@ -19,6 +19,7 @@ package v1alpha2
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -50,14 +51,8 @@ func applyProfile(config *SkaffoldConfig, profile Profile) {
 	*config = SkaffoldConfig{
 		APIVersion: config.APIVersion,
 		Kind:       config.Kind,
-		Build: BuildConfig{
-			Artifacts: overlayProfileField(config.Build.Artifacts, profile.Build.Artifacts).([]*Artifact),
-			TagPolicy: overlayProfileField(config.Build.TagPolicy, profile.Build.TagPolicy).(TagPolicy),
-			BuildType: overlayProfileField(config.Build.BuildType, profile.Build.BuildType).(BuildType),
-		},
-		Deploy: DeployConfig{
-			DeployType: overlayProfileField(config.Deploy.DeployType, profile.Deploy.DeployType).(DeployType),
-		},
+		Build:      overlayProfileField(config.Build, profile.Build).(BuildConfig),
+		Deploy:     overlayProfileField(config.Deploy, profile.Deploy).(DeployConfig),
 	}
 }
 
@@ -69,24 +64,62 @@ func profilesByName(profiles []Profile) map[string]Profile {
 	return byName
 }
 
+func isOneOf(field reflect.StructField) bool {
+	for _, tag := range strings.Split(field.Tag.Get("yamltags"), ",") {
+		if tag == "oneOf" {
+			return true
+		}
+	}
+	return false
+}
+
+// if we find a oneOf tag, the fields in this struct are themselves pointers to structs,
+// but should be treated as values. the first non-nil one we find is what we should use.
+func overlayOneOfField(config interface{}, profile interface{}) interface{} {
+	v := reflect.ValueOf(profile) // the profile itself
+	t := reflect.TypeOf(profile)  // the type of the profile, used for getting struct field types
+	for i := 0; i < v.NumField(); i++ {
+		fieldType := t.Field(i)              // the field type (e.g. 'LocalBuild' for BuildConfig)
+		fieldValue := v.Field(i).Interface() // the value of the field itself
+
+		if fieldValue != nil && !reflect.ValueOf(fieldValue).IsNil() {
+			ret := reflect.New(t)                                                   // New(t) returns a Value representing pointer to new zero value for type t
+			ret.Elem().FieldByName(fieldType.Name).Set(reflect.ValueOf(fieldValue)) // set the value
+			return reflect.Indirect(ret).Interface()                                // since ret is a pointer, dereference it
+		}
+	}
+	// if we're here, we didn't find any values set in the profile config. just return the original.
+	logrus.Infof("no values found in profile for field %s, using original config values", t.Name())
+	return config
+}
+
+func overlayStructField(config interface{}, profile interface{}) interface{} {
+	// we already know the top level fields for whatever struct we have are themselves structs
+	// (and not one-of values), so we need to recursively overlay them
+	configValue := reflect.ValueOf(config)
+	profileValue := reflect.ValueOf(profile)
+	t := reflect.TypeOf(profile)
+	finalConfig := reflect.New(t)
+
+	for i := 0; i < profileValue.NumField(); i++ {
+		fieldType := t.Field(i)
+		overlay := overlayProfileField(configValue.Field(i).Interface(), profileValue.Field(i).Interface())
+		finalConfig.Elem().FieldByName(fieldType.Name).Set(reflect.ValueOf(overlay))
+	}
+	return reflect.Indirect(finalConfig).Interface() // since finalConfig is a pointer, dereference it
+}
+
 func overlayProfileField(config interface{}, profile interface{}) interface{} {
 	v := reflect.ValueOf(profile) // the profile itself
 	t := reflect.TypeOf(profile)  // the type of the profile, used for getting struct field types
 	logrus.Debugf("overlaying profile on config for field %s", t.Name())
 	switch v.Kind() {
 	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			fieldType := t.Field(i)              // the field type (e.g. 'LocalBuild' for BuildConfig)
-			fieldValue := v.Field(i).Interface() // the value of the field itself
-			if fieldValue != nil && !reflect.ValueOf(fieldValue).IsNil() {
-				ret := reflect.New(t)                                                   // New(t) returns a Value representing pointer to new zero value for type t
-				ret.Elem().FieldByName(fieldType.Name).Set(reflect.ValueOf(fieldValue)) // set the value
-				return reflect.Indirect(ret).Interface()                                // since ret is a pointer, dereference it
-			}
+		// check the first field of the struct for a oneOf yamltag.
+		if isOneOf(t.Field(0)) {
+			return overlayOneOfField(config, profile)
 		}
-		// if we're here, we didn't find any values set in the profile config. just return the original.
-		logrus.Infof("no values found in profile for field %s, using original config values", t.Name())
-		return config
+		return overlayStructField(config, profile)
 	case reflect.Slice:
 		// either return the values provided in the profile, or the original values if none were provided.
 		if v.Len() == 0 {
