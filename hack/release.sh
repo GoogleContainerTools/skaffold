83e71c97136021c00199bf6b393df56848d54113
Matt Rickard <m@rickard.email>
2018-09-24 17:43:22 -0700

    rename release_notes.sh to release.sh

--- hack/release.sh
+++ hack/release.sh
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-EXAMPLES_DIR=${DIR}/../../examples
-INTEGRATION_EXAMPLES_DIR=${DIR}/../../integration/examples
+EXAMPLES_DIR=${DIR}/../examples
+INTEGRATION_EXAMPLES_DIR=${DIR}/../integration/examples
 
 # you can pass your github token with --token here if you run out of requests
-go run hack/release_notes/listpullreqs.go
+go run ${DIR}/release_notes/listpullreqs.go
 
 # sync files from integration examples to examples/
 rm -rf ${EXAMPLES_DIR} && rm -rf ${INTEGRATION_EXAMPLES_DIR}/bazel/bazel-* && cp -r ${INTEGRATION_EXAMPLES_DIR} ${EXAMPLES_DIR}
