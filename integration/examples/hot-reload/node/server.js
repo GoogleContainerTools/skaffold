cc3e9dbb31fb604791a56905210ce576eb028492
Matt Rickard <mrick@google.com>
2018-09-28 14:56:07 -0500

    [examples] add flask hot-reload example

--- integration/examples/hot-reload/node/server.js
+++ integration/examples/hot-reload/node/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World?\n');
+  res.end('Hello World from Node!\n');
 });
 
 server.listen(port, hostname, () => {
