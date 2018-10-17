5e65832ff214900bb056e365bad14c2bd36ac4e7
David Gageot <david@gageot.net>
2018-10-17 07:00:41 +0200

    Add tests for the log level
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/build/kaniko/run.go
+++ pkg/skaffold/build/kaniko/run.go
@@ -20,21 +20,15 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/build/kaniko/sources"
-	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/constants"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/docker"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/kubernetes"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 func (b *Builder) run(ctx context.Context, out io.Writer, artifact *latest.Artifact, cfg *latest.KanikoBuild) (string, error) {
@@ -87,42 +81,4 @@ func (b *Builder) run(ctx context.Context, out io.Writer, artifact *latest.Artif
 	waitForLogs()
 
 	return imageDst, nil
-}
-
-func logLevel() logrus.Level {
-	level := logrus.GetLevel()
-	if level < logrus.InfoLevel {
-		return logrus.InfoLevel
-	}
-	return level
-}
-
-func streamLogs(out io.Writer, name string, pods corev1.PodInterface) func() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	var retry int32 = 1
-	go func() {
-		defer wg.Done()
-
-		for atomic.LoadInt32(&retry) == 1 {
-			r, err := pods.GetLogs(name, &v1.PodLogOptions{
-				Follow:    true,
-				Container: constants.DefaultKanikoContainerName,
-			}).Stream()
-			if err != nil {
-				logrus.Debugln("unable to get kaniko pod logs:", err)
-				time.Sleep(1 * time.Second)
-				continue
-			}
-
-			io.Copy(out, r)
-			return
-		}
-	}()
-
-	return func() {
-		atomic.StoreInt32(&retry, 0)
-		wg.Wait()
-	}
 }
