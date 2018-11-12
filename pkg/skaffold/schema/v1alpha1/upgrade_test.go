ee31bb564e68c8de39da05d8451418d5c4a27aca
Nick Kubala <nkubala@google.com>
2018-11-12 14:26:24 -0800

    add unit tests for v1alpha2 and v1alpha3

---
+++ pkg/skaffold/schema/v1alpha1/upgrade_test.go
@@ -0,0 +1,127 @@
+/*
+Copyright 2018 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"testing"
+
+	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v1alpha2"
+	"github.com/GoogleContainerTools/skaffold/testutil"
+)
+
+func TestPipelineUpgrade(t *testing.T) {
+	tests := []struct {
+		name     string
+		yaml     string
+		expected *next.SkaffoldPipeline
+	}{
+		{
+			name: "git tagger",
+			yaml: `apiVersion: skaffold/v1alpha1
+kind: Config
+build:
+  tagPolicy: gitCommit
+`,
+			expected: &next.SkaffoldPipeline{
+				APIVersion: next.Version,
+				Kind:       "Config",
+				Build: next.BuildConfig{
+					TagPolicy: next.TagPolicy{
+						GitTagger: &next.GitTagger{},
+					},
+					Artifacts: []*next.Artifact{},
+				},
+			},
+		},
+		{
+			name: "sha tagger",
+			yaml: `apiVersion: skaffold/v1alpha1
+kind: Config
+build:
+  tagPolicy: sha256
+`,
+			expected: &next.SkaffoldPipeline{
+				APIVersion: next.Version,
+				Kind:       "Config",
+				Build: next.BuildConfig{
+					TagPolicy: next.TagPolicy{
+						ShaTagger: &next.ShaTagger{},
+					},
+					Artifacts: []*next.Artifact{},
+				},
+			},
+		},
+		{
+			name: "normal skaffold yaml",
+			yaml: `apiVersion: skaffold/v1alpha1
+kind: Config
+build:
+  artifacts:
+  - imageName: gcr.io/k8s-skaffold/skaffold-example
+deploy:
+  kubectl:
+    manifests:
+    - paths:
+      - k8s-*
+`,
+			expected: &next.SkaffoldPipeline{
+				APIVersion: next.Version,
+				Kind:       "Config",
+				Build: next.BuildConfig{
+					TagPolicy: next.TagPolicy{
+						GitTagger: &next.GitTagger{},
+					},
+					Artifacts: []*next.Artifact{
+						&next.Artifact{
+							ImageName: "gcr.io/k8s-skaffold/skaffold-example",
+							ArtifactType: next.ArtifactType{
+								DockerArtifact: &next.DockerArtifact{},
+							},
+						},
+					},
+				},
+				Deploy: next.DeployConfig{
+					DeployType: next.DeployType{
+						KubectlDeploy: &next.KubectlDeploy{
+							Manifests: []string{
+								"k8s-*",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pipeline := NewSkaffoldPipeline()
+			err := pipeline.Parse([]byte(tt.yaml), true)
+			if err != nil {
+				t.Fatalf("unexpected error during parsing old config: %v", err)
+			}
+
+			upgraded, err := pipeline.Upgrade()
+			if err != nil {
+				t.Errorf("unexpected error during upgrade: %v", err)
+			}
+
+			upgradedPipeline := upgraded.(*next.SkaffoldPipeline)
+			testutil.CheckDeepEqual(t, tt.expected, upgradedPipeline)
+		})
+	}
+}
