4f5c748996c42bf91ffadc8cd67427f7b8ab7177
balopat <balintp@google.com>
2018-11-16 16:32:53 -0800

    flattening submodule

---
+++ docs/themes/docsy-skaffold/postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsers: [
+                "Android 2.3",
+                "Android >= 4",
+                "Chrome >= 20",
+                "Firefox >= 24",
+                "Explorer >= 8",
+                "iOS >= 6",
+                "Opera >= 12",
+                "Safari >= 6"
+            ]
+        }
+    },
+}
