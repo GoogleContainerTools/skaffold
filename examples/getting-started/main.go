5a8a541bcafb3e7d9b038c6f9209ac38b9a3b8d1
Christie Wilson <bobcatfish@gmail.com>
2018-12-18 11:29:17 -0800

    Change getting started to emojinetes
    Instead of displaying "hello world", with this change the example will
    cycle through emojis and a rainbow of terminal colors. Not sure if we
    should make this the default example, since the terminal escape colors
    will look terrible if looking at logs directly (e.g. piping into a file,
    offloading themsomewhere)
    
    This is based on the Google skaffold OSS ecosystem booth demo from
    Kubecon Seattle 2018

--- examples/getting-started/main.go
+++ examples/getting-started/main.go
@@ -5,9 +5,30 @@ import (
 	"time"
 )
 
+const (
+	startingEmoji = '🐰'
+	maxEmoji      = 200
+
+	colorReset = "\u001b[0m"
+)
+
+var colors = []string{
+	"\u001b[31;1m", // red bold
+	"\u001b[33;1m", // yellow bold
+	"\u001b[32;1m", // green bold
+	"\u001b[34;1m", // blue bold
+	"\u001b[36;1m", // cyan bold
+	"\u001b[35;1m", // magenta bold
+}
+
 func main() {
 	for {
-		fmt.Println("Hello world!")
+		s := ""
+		for i := 0; i < maxEmoji; i++ {
+			s += fmt.Sprintf("%c%sHELLO WORLD%s", startingEmoji+i, colors[i%len(colors)], colorReset)
+		}
+
+		fmt.Println(s)
 
 		time.Sleep(time.Second * 1)
 	}
