4c6e45b4f35199dbb910a46210ad4c370e92d7fe
David Gageot <david@gageot.net>
2019-01-16 11:35:05 -0500

    Introduce v1beta3
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/schema/v1beta2/config.go
+++ pkg/skaffold/schema/v1beta2/config.go
@@ -14,13 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package latest
+package v1beta2
 
 import (
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/util"
 )
 
-const Version string = "skaffold/v1beta3"
+const Version string = "skaffold/v1beta2"
 
 // NewSkaffoldPipeline creates a SkaffoldPipeline
 func NewSkaffoldPipeline() util.VersionedConfig {
--- pkg/skaffold/schema/v1beta2/upgrade_test.go
+++ pkg/skaffold/schema/v1beta2/upgrade_test.go
@@ -14,18 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta1
+package v1beta2
 
 import (
 	"testing"
 
-	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v1beta2"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 	"github.com/GoogleContainerTools/skaffold/testutil"
 	yaml "gopkg.in/yaml.v2"
 )
 
 func TestUpgrade(t *testing.T) {
-	yaml := `apiVersion: skaffold/v1beta1
+	yaml := `apiVersion: skaffold/v1beta2
 kind: Config
 build:
   artifacts:
@@ -58,7 +58,7 @@ profiles:
         manifests:
         - k8s-*
 `
-	expected := `apiVersion: skaffold/v1beta2
+	expected := `apiVersion: skaffold/v1beta3
 kind: Config
 build:
   artifacts:
@@ -80,8 +80,6 @@ profiles:
         pullSecretName: e2esecret
         namespace: default
         cache: {}
-        # default should be no flags at upgrade
-        flags: 
       artifacts:
       - image: gcr.io/k8s-skaffold/skaffold-example
     test:
@@ -104,7 +102,7 @@ func verityUpgrade(t *testing.T, input, output string) {
 	upgraded, err := pipeline.Upgrade()
 	testutil.CheckError(t, false, err)
 
-	expected := v1beta2.NewSkaffoldPipeline()
+	expected := latest.NewSkaffoldPipeline()
 	err = yaml.UnmarshalStrict([]byte(output), expected)
 
 	testutil.CheckErrorAndDeepEqual(t, false, err, expected, upgraded)
