9a2a370b61948a89e08cf1a51864af7f715b5ad2
Priya Wadhwa <priyawadhwa@google.com>
2019-02-01 16:21:27 -0800

    Allow user to specify custom kaniko image
    fixes #1586, previously we were using the default image instead of the image passed in through the config.

--- pkg/skaffold/build/kaniko/sources/sources.go
+++ pkg/skaffold/build/kaniko/sources/sources.go
@@ -58,7 +58,7 @@ func podTemplate(cfg *latest.KanikoBuild, args []string) *v1.Pod {
 			Containers: []v1.Container{
 				{
 					Name:            constants.DefaultKanikoContainerName,
-					Image:           constants.DefaultKanikoImage,
+					Image:           cfg.Image,
 					Args:            args,
 					ImagePullPolicy: v1.PullIfNotPresent,
 					Env: []v1.EnvVar{{
