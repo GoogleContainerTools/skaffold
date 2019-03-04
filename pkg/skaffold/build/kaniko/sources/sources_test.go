0a913a4a2f0e1acb3f2cd15c8fb2361055899a6e
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-04 19:50:24 +0000

    feat: added tests to validate pod creation

---
+++ pkg/skaffold/build/kaniko/sources/sources_test.go
@@ -0,0 +1,216 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sources
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
+	"github.com/GoogleContainerTools/skaffold/testutil"
+)
+
+func TestPodTemplate(t *testing.T) {
+	tests := []struct {
+		name     string
+		initial  *latest.KanikoBuild
+		args     []string
+		expected *v1.Pod
+	}{
+		{
+			name:    "basic pod",
+			initial: &latest.KanikoBuild{},
+			expected: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "kaniko-",
+					Labels:       map[string]string{"skaffold-kaniko": "skaffold-kaniko"},
+				},
+				Spec: v1.PodSpec{
+					RestartPolicy: "Never",
+					Containers: []v1.Container{
+						{
+							Name: "kaniko",
+							Env: []v1.EnvVar{
+								{
+									Name:  "GOOGLE_APPLICATION_CREDENTIALS",
+									Value: "/secret/kaniko-secret",
+								},
+							},
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "kaniko-secret", MountPath: "/secret",
+								},
+							},
+							ImagePullPolicy: v1.PullPolicy("IfNotPresent"),
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "kaniko-secret",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "with docker config",
+			initial: &latest.KanikoBuild{
+				DockerConfig: &latest.DockerConfig{
+					SecretName: "docker-cfg",
+					Path:       "/kaniko/.docker",
+				},
+			},
+			expected: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "kaniko-",
+					Labels:       map[string]string{"skaffold-kaniko": "skaffold-kaniko"},
+				},
+				Spec: v1.PodSpec{
+					RestartPolicy: "Never",
+					Containers: []v1.Container{
+						{
+							Name: "kaniko",
+							Env: []v1.EnvVar{
+								{
+									Name:  "GOOGLE_APPLICATION_CREDENTIALS",
+									Value: "/secret/kaniko-secret",
+								},
+							},
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "kaniko-secret", MountPath: "/secret",
+								},
+								{
+									Name: "docker-cfg", MountPath: "/kaniko/.docker",
+								},
+							},
+							ImagePullPolicy: v1.PullPolicy("IfNotPresent"),
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "kaniko-secret",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "",
+								},
+							},
+						},
+						{
+							Name: "docker-cfg",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "docker-cfg",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "with resource constraints",
+			initial: &latest.KanikoBuild{
+				Resources: &latest.ResourceRequirements{
+					Requests: &latest.ResourceRequirement{
+						CPU:    "0.5",
+						Memory: "1000",
+					},
+					Limits: &latest.ResourceRequirement{
+						CPU:    "1.0",
+						Memory: "2000",
+					},
+				},
+			},
+			expected: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "kaniko-",
+					Labels:       map[string]string{"skaffold-kaniko": "skaffold-kaniko"},
+				},
+				Spec: v1.PodSpec{
+					RestartPolicy: "Never",
+					Containers: []v1.Container{
+						{
+							Name: "kaniko",
+							Env: []v1.EnvVar{
+								{
+									Name:  "GOOGLE_APPLICATION_CREDENTIALS",
+									Value: "/secret/kaniko-secret",
+								},
+							},
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "kaniko-secret", MountPath: "/secret",
+								},
+							},
+							ImagePullPolicy: v1.PullPolicy("IfNotPresent"),
+							Resources: createResourceRequirements(
+								resource.MustParse("1.0"),
+								resource.MustParse("2000"),
+								resource.MustParse("0.5"),
+								resource.MustParse("1000")),
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "kaniko-secret",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	opt := cmp.Comparer(func(x, y resource.Quantity) bool {
+		return x.String() == y.String()
+	})
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := podTemplate(test.initial, test.args)
+			testutil.CheckDeepEqual(t, test.expected, actual, opt)
+		})
+	}
+}
+
+func createResourceRequirements(cpuLimit resource.Quantity, memoryLimit resource.Quantity, cpuRequest resource.Quantity, memoryRequest resource.Quantity) v1.ResourceRequirements {
+	req := v1.ResourceRequirements{}
+	req.Limits = v1.ResourceList{}
+	req.Limits[v1.ResourceCPU] = cpuLimit
+	req.Limits[v1.ResourceMemory] = memoryLimit
+
+	req.Requests = v1.ResourceList{}
+	req.Requests[v1.ResourceCPU] = cpuRequest
+	req.Requests[v1.ResourceMemory] = memoryRequest
+
+	return req
+
+}
