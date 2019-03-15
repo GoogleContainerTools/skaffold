48f109215aaf5b5e44189fa575190e8f1710ce7b
Brian de Alwis <bsd@mt.ca>
2019-03-15 14:35:00 -0400

    simplify debug() by delegating to dev()

--- cmd/skaffold/app/cmd/debug.go
+++ cmd/skaffold/app/cmd/debug.go
@@ -17,15 +17,10 @@ limitations under the License.
 package cmd
 
 import (
-	"context"
 	"io"
 
-	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/config"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/debugging"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/deploy"
-	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/runner"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -46,22 +41,6 @@ func NewCmdDebug(out io.Writer) *cobra.Command {
 }
 
 func debug(out io.Writer) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	catchCtrlC(cancel)
-
-	cleanup := func() {}
-	if opts.Cleanup {
-		defer func() {
-			cleanup()
-		}()
-	}
-
-	output := &config.Output{
-		Main: out,
-		Logs: out,
-	}
-
 	// HACK: disable watcher to prevent redeploying changed containers during debugging
 	// TODO: avoid redeploys of debuggable artifacts, but still enable file-sync
 	if len(opts.TargetImages) == 0 {
@@ -70,30 +49,5 @@ func debug(out io.Writer) error {
 
 	deploy.AddManifestTransform(debugging.ApplyDebuggingTransforms)
 
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-			r, config, err := newRunner(opts)
-			if err != nil {
-				return errors.Wrap(err, "creating runner")
-			}
-
-			// follow the normal dev flow
-			err = r.Dev(ctx, output, config.Build.Artifacts)
-			if r.HasDeployed() {
-				cleanup = func() {
-					if err := r.Cleanup(context.Background(), out); err != nil {
-						logrus.Warnln("cleanup:", err)
-					}
-				}
-			}
-			if err != nil {
-				if errors.Cause(err) != runner.ErrorConfigurationChanged {
-					return err
-				}
-			}
-		}
-	}
+	return dev(out, opts.ExperimentalGUI)
 }
