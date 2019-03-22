6b8328f2eaed856c0794a516187e282c4554437f
balopat <balintp@google.com>
2019-03-22 12:03:45 -0700

    adding all the other flags

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ GREEN='\033[0;32m'
 RESET='\033[0m'
 
 for i in `seq 100`; do
-    echo $i ; go test -count=1 github.com/GoogleContainerTools/skaffold/pkg/skaffold/watch ;
+    echo $i ; go test -count=1 -race -cover -short -timeout=60s -coverprofile=out/coverage.txt -coverpkg="./pkg/...,./cmd/..." ./... github.com/GoogleContainerTools/skaffold/pkg/skaffold/watch ;
 done
 
 
