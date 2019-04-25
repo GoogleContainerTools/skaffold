8b86017c2c7c9b4eb6e620a35792e12be0d7bde7
Cornelius Weig <22861411+corneliusweig@users.noreply.github.com>
2019-04-25 21:56:46 +0200

    Move addition to v1beta8

--- pkg/skaffold/schema/v1beta8/upgrade.go
+++ pkg/skaffold/schema/v1beta8/upgrade.go
@@ -25,7 +25,8 @@ import (
 
 // Upgrade upgrades a configuration to the next version.
 // Config changes from v1beta8 to v1beta9
-// 1. No additions
+// 1. Additions:
+//    gitTagger/variant
 // 2. No removals
 // 3. No updates
 func (config *SkaffoldConfig) Upgrade() (util.VersionedConfig, error) {
