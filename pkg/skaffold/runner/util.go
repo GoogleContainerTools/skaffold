39d2a8761c4021f36c7428b507343cb5820a2122
Tejal Desai <tejaldesai@google.com>
2019-05-08 18:16:07 -0700

    add the missed util file

---
+++ pkg/skaffold/runner/util.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package runner
+
+import (
+	"sync"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/build"
+)
+
+// ProcessBuildResults waits for n build results to be written on channel ch
+func ProcessBuildResults(ch chan build.Result, n int) {
+	wg := &sync.WaitGroup{}
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(i int, c chan build.Result) {
+			defer wg.Done()
+			<-c
+			// Do event api work here
+		}(i, ch)
+	}
+	wg.Wait()
+	return
+}
