0858a8e1e6ad61343e6d59ad2ad5756c9ffc892e
David Gageot <david@gageot.net>
2019-05-15 14:10:36 +0200

    Remove dead code
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/util/util.go
+++ pkg/skaffold/util/util.go
@@ -30,7 +30,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"runtime"
 	"sort"
 	"strings"
 
@@ -44,15 +43,6 @@ const (
 
 func RandomID() string {
 	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		panic(err)
-	}
-	return fmt.Sprintf("%x", b)
-}
-
-func RandomFourCharacterID() string {
-	b := make([]byte, 2)
 	_, err := rand.Read(b)
 	if err != nil {
 		panic(err)
@@ -131,17 +121,6 @@ func ExpandPathsGlob(workingDir string, paths []string) ([]string, error) {
 	}
 	sort.Strings(ret)
 	return ret, nil
-}
-
-// HasMeta reports whether path contains any of the magic characters
-// recognized by filepath.Match.
-// This is a copy of filepath/match.go's hasMeta
-func HasMeta(path string) bool {
-	magicChars := `*?[`
-	if runtime.GOOS != "windows" {
-		magicChars = `*?[\`
-	}
-	return strings.ContainsAny(path, magicChars)
 }
 
 // BoolPtr returns a pointer to a bool
