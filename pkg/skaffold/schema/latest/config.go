9a6d4e7afa20e556bf264e7ce6c5d963c27ba9d4
David Gageot <david@gageot.net>
2019-06-19 14:19:57 +0200

    Docker is case sensitive about networks
    host/bridge/none must be lower case or will be treated as network names.
    
    This is a follow up on @tomaratyn’s work
    
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/schema/latest/config.go
+++ pkg/skaffold/schema/latest/config.go
@@ -672,9 +672,9 @@ type DockerArtifact struct {
 	// NetworkMode is passed through to docker and overrides the
 	// network configuration of docker builder. If unset, use whatever
 	// is configured in the underlying docker daemon. Valid modes are
-	// `Host`: use the host's networking stack.
-	// `Bridge`: use the bridged network configuration.
-	// `None`: no networking in the container.
+	// `host`: use the host's networking stack.
+	// `bridge`: use the bridged network configuration.
+	// `none`: no networking in the container.
 	NetworkMode string `yaml:"network,omitempty"`
 
 	// CacheFrom lists the Docker images used as cache sources.
