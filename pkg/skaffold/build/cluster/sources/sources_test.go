db77b5e8f6e2ae0927bdec40c13f3873d39872be
Prashant Arya <prary2@gmail.com>
2019-07-04 08:31:31 +0530

    fixing

--- pkg/skaffold/build/cluster/sources/sources_test.go
+++ pkg/skaffold/build/cluster/sources/sources_test.go
@@ -273,7 +273,7 @@ func TestSetProxy(t *testing.T) {
 			description:    "set https proxy",
 			clusterDetails: &latest.ClusterDetails{HTTPSProxy: "proxy.com"},
 			env:            []v1.EnvVar{},
-			expectedArgs:   []v1.EnvVar{{Name: "HTTP_PROXY", Value: "proxy.com"}},
+			expectedArgs:   []v1.EnvVar{{Name: "HTTPS_PROXY", Value: "proxy.com"}},
 		}, {
 			description:    "set http and https proxy",
 			clusterDetails: &latest.ClusterDetails{HTTPProxy: "proxy.com", HTTPSProxy: "proxy.com"},
