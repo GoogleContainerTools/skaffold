24fcb4e1b9a981d5a237bb8830bcbb234a3b0114
balopat <balintp@google.com>
2019-07-18 18:50:54 -0700

    boilerplate + lint

--- pkg/skaffold/kubernetes/portforward/port_forward_integration.go
+++ pkg/skaffold/kubernetes/portforward/port_forward_integration.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package portforward
 
 import (
@@ -11,8 +27,9 @@ import (
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 )
 
+//For WhiteBox testing only
 //This is testing a port forward + stop + restart in a simulated dev cycle
-func WhiteBox_PortForwardCycle(namespace string, t *testing.T) {
+func WhiteBoxPortForwardCycle(namespace string, t *testing.T) {
 	em := NewEntryManager(os.Stdout)
 	portForwardEvent = func(entry *portForwardEntry) {}
 	ctx := context.Background()
