8530514a6d62b85878a61bc3463ab49c8f2806d3
Marlon Gamez <marlongamez@google.com>
2019-07-24 12:43:48 -0700

    cleaner pipeline output, code cleanup

--- pkg/skaffold/runner/generate_pipeline.go
+++ pkg/skaffold/runner/generate_pipeline.go
@@ -19,6 +19,7 @@ package runner
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -26,9 +27,11 @@ import (
 
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 	"github.com/pkg/errors"
-	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
-	"gopkg.in/yaml.v2"
+	//"gopkg.in/yaml.v2"
+	"github.com/ghodss/yaml"
 	"k8s.io/api/core/v1"
+
+	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -38,18 +41,105 @@ func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, co
 	}
 
 	var output bytes.Buffer
-	encoder := yaml.NewEncoder(&output)
-	defer encoder.Close()
 
+	gitResource, err := generateGitResource(config)
+	if err != nil {
+		return errors.Wrap(err, "generating git resource for pipeline")
+	}
+
+	var tasks []tekton.Task
+	taskBuild, err := generateBuildTask(config)
+	if err == nil {
+		tasks = append(tasks, taskBuild)
+	}
+	taskDeploy, err := generateDeployTask(config)
+	if err == nil {
+		tasks = append(tasks, taskDeploy)
+	}
+
+	// Create pipeline to tie together all tasks
+	pipeline := tekton.Pipeline{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pipeline",
+			APIVersion: "tekton.dev/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "skaffold-pipeline",
+		},
+		Spec: tekton.PipelineSpec{
+			Resources: []tekton.PipelineDeclaredResource{
+				{
+					Name: "source-repo",
+					Type: tekton.PipelineResourceTypeGit,
+				},
+			},
+			Tasks: []tekton.PipelineTask{},
+		},
+	}
+	// Create tasks in pipeline spec for all corresponding tasks
+	for _, task := range tasks {
+		pipelineTask := tekton.PipelineTask{
+			Name: fmt.Sprintf("%s-task", task.Name),
+			TaskRef: tekton.TaskRef{
+				Name: task.Name,
+			},
+			Resources: &tekton.PipelineTaskResources{
+				Inputs: []tekton.PipelineTaskInputResource{
+					{
+						Name:     "source",
+						Resource: "source-repo",
+					},
+				},
+			},
+		}
+		pipeline.Spec.Tasks = append(pipeline.Spec.Tasks, pipelineTask)
+	}
+
+	// json.Marshal all pieces of pipeline, then convert all to yaml and write them to file
+	var jsons [][]byte
+
+	bGitResource, err := json.Marshal(gitResource)
+	if err != nil {
+		return errors.Wrap(err, "marshaling git resource")
+	}
+	jsons = append(jsons, bGitResource)
+	for _, task := range tasks {
+		bTask, err := json.Marshal(task)
+		if err != nil {
+			return errors.Wrap(err, "marshaling task")
+		}
+		jsons = append(jsons, bTask)
+	}
+	bPipeline, err := json.Marshal(pipeline)
+	if err != nil {
+		return errors.Wrap(err, "marshaling pipeline")
+	}
+	jsons = append(jsons, bPipeline)
+
+	for _, item := range jsons {
+		itemYaml, err := yaml.JSONToYAML(item)
+		if err != nil {
+			return errors.Wrap(err, "converting json to yaml")
+		}
+		output.Write(append(itemYaml, []byte("---\n")...))
+	}
+	if err := ioutil.WriteFile(fileOut, output.Bytes(), 0755); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func generateGitResource(config *latest.SkaffoldConfig) (tekton.PipelineResource, error) {
 	// Get git repo url
 	getGitRepo := exec.Command("git", "config", "--get", "remote.origin.url")
 	gitRepo, err := getGitRepo.Output()
 	if err != nil {
-		return errors.Wrap(err, "getting git repo from git config")
+		return tekton.PipelineResource{}, errors.Wrap(err, "getting git repo from git config")
 	}
 
 	// Create git resource for pipeline from users current git repo
-	gitResource := tekton.PipelineResource{
+	return tekton.PipelineResource{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "PipelineResource",
 			APIVersion: "tekton.dev/v1alpha1",
@@ -66,45 +156,49 @@ func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, co
 				},
 			},
 		},
+	}, nil
+}
+
+func generateBuildTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
+	if len(config.Pipeline.Build.Artifacts) == 0 {
+		return tekton.Task{}, errors.New("No artifacts to build")
 	}
-
-	//TODO(marlon-gamez): Check if config.pipeline.Build.Artifacts exists
-
-	// Create build tasks for all artifacts listed in skaffold build config
-	var tasks []tekton.Task
-	for i := range config.Pipeline.Build.Artifacts {
-		taskBuild := tekton.Task{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "Task",
-				APIVersion: "tekton.dev/v1alpha1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name: fmt.Sprintf("skaffold-build-%d", i),
-			},
-			Spec: tekton.TaskSpec{
-				Inputs: &tekton.Inputs{
-					Resources: []tekton.TaskResource{
-						{
-							Name: "source",
-							Type: tekton.PipelineResourceTypeGit,
-						},
-					},
-				},
-				Steps: []v1.Container{
+	return tekton.Task{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Task",
+			APIVersion: "tekton.dev/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "skaffold-build",
+		},
+		Spec: tekton.TaskSpec{
+			Inputs: &tekton.Inputs{
+				Resources: []tekton.TaskResource{
 					{
-						Name:    "run-build",
-						Image:   "gcr.io/k8s-skaffold/skaffold:v0.33.0",
-						Command: []string{"skaffold build"},
-						Args:    []string{"--filename", "/workspace/source/pipeline/skaffold.yaml"},
+						Name: "source",
+						Type: tekton.PipelineResourceTypeGit,
 					},
 				},
 			},
-		}
-		tasks = append(tasks, taskBuild)
+			Steps: []v1.Container{
+				{
+					Name:    "run-build",
+					Image:   "gcr.io/k8s-skaffold/skaffold:v0.33.0",
+					Command: []string{"skaffold build"},
+					Args:    []string{"--filename", "/workspace/source/pipeline/skaffold.yaml"},
+				},
+			},
+		},
+	}, nil
+}
+
+func generateDeployTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
+	deployConfig := config.Pipeline.Deploy
+	if deployConfig.HelmDeploy == nil && deployConfig.KubectlDeploy == nil && deployConfig.KustomizeDeploy == nil {
+		return tekton.Task{}, errors.New("No Help/Kubectl/Kustomize deploy config")
 	}
 
-	//TODO(marlon-gamez): Check if config.pipeline.Deploy.HelmDeploy/KubectlDeploy/KustomizeDeploy exist
-	taskDeploy := tekton.Task{
+	return tekton.Task{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Task",
 			APIVersion: "tekton.dev/v1alpha1",
@@ -130,77 +224,5 @@ func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, co
 				},
 			},
 		},
-	}
-	tasks = append(tasks, taskDeploy)
-
-	// Create pipeline to tie together all tasks
-	pipeline := tekton.Pipeline{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pipeline",
-			APIVersion: "tekton.dev/v1alpha1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "skaffold-pipeline",
-		},
-		Spec: tekton.PipelineSpec{
-			Resources: []tekton.PipelineDeclaredResource{
-				{
-					Name: "source-repo",
-					Type: tekton.PipelineResourceTypeGit,
-				},
-			},
-			Tasks: []tekton.PipelineTask{
-				{
-					Name: "skaffold-build-task",
-					TaskRef: tekton.TaskRef{
-						Name: "skaffold-build",
-					},
-					Resources: &tekton.PipelineTaskResources{
-						Inputs: []tekton.PipelineTaskInputResource{
-							{
-								Name:     "source",
-								Resource: "source-repo",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	// Create tasks in pipeline spec for all corresponding tasks
-	for i, task := range tasks {
-		pipelineTask := tekton.PipelineTask{
-			Name: fmt.Sprintf("skaffold-build-task-%d", i),
-			TaskRef: tekton.TaskRef{
-				Name: task.Name,
-			},
-			Resources: &tekton.PipelineTaskResources{
-				Inputs: []tekton.PipelineTaskInputResource{
-					{
-						Name:     "source",
-						Resource: "source-repo",
-					},
-				},
-			},
-		}
-		pipeline.Spec.Tasks = append(pipeline.Spec.Tasks, pipelineTask)
-	}
-
-	// Encode all pieces of pipeline to out buffer then write buffer to file
-	if err := encoder.Encode(gitResource); err != nil {
-		return errors.Wrap(err, "encoding git resource")
-	}
-	for _, task := range tasks {
-		if err := encoder.Encode(task); err != nil {
-			return errors.Wrap(err, "encoding task")
-		}
-	}
-	if err := encoder.Encode(pipeline); err != nil {
-		return errors.Wrap(err, "encoding pipeline")
-	}
-	if err := ioutil.WriteFile(fileOut, output.Bytes(), 0755); err != nil {
-		return err
-	}
-	return nil
-
+	}, nil
 }
