d4d23340aae2e8cd55b5e774a25952fb6c32c854
Marlon Gamez <marlongamez@google.com>
2019-07-29 15:02:26 -0700

    code cleanup + unit testing

--- pkg/skaffold/runner/generate_pipeline.go
+++ pkg/skaffold/runner/generate_pipeline.go
@@ -28,94 +28,62 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
 	yamlv2 "gopkg.in/yaml.v2"
 	"k8s.io/api/core/v1"
 
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
+
 	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, config *latest.SkaffoldConfig, fileOut string) error {
-	if config.APIVersion == "" || config.Kind == "" {
-		return errors.New("Invalid skaffold config")
-	}
+var (
+	// for testing
+	inputReader = bufio.NewReader(os.Stdin)
+)
 
-	reader := bufio.NewReader(os.Stdin)
-	err := createSkaffoldProfile(config, reader)
+type StringReader interface {
+	ReadString(delim byte) (string, error)
+}
+
+func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, config *latest.SkaffoldConfig, fileOut string) error {
+	err := createSkaffoldProfile(config, "skaffold.yaml")
 	if err != nil {
 		return errors.Wrap(err, "setting up profile")
 	}
 
 	fmt.Println("Generating Pipeline...")
 
-	var output bytes.Buffer
-	gitResource, err := generateGitResource(config)
+	// Generate git resource for pipeline
+	gitResource, err := generateGitResource()
 	if err != nil {
-		fmt.Print("Could not get git url automatically, please enter: ")
-		newUrl, err := reader.ReadString('\n')
-		if err != nil {
-			return errors.Wrap(err, "generating git resource for pipeline")
-		}
-		gitResource.Spec.Params[0].Value = newUrl
+		return errors.Wrap(err, "generating git resource for pipeline")
 	}
 
-	var tasks []tekton.Task
-	taskBuild, err := generateBuildTask(config)
-	if err == nil {
-		tasks = append(tasks, taskBuild)
+	// Generate build task for pipeline
+	var tasks []*tekton.Task
+	taskBuild, err := generateBuildTask(config.Pipeline.Build)
+	if err != nil {
+		return errors.Wrap(err, "generating build task")
 	}
-	taskDeploy, err := generateDeployTask(config)
-	if err == nil {
-		tasks = append(tasks, taskDeploy)
+	tasks = append(tasks, taskBuild)
+
+	// Generate deploy task for pipeline
+	taskDeploy, err := generateDeployTask(config.Pipeline.Deploy)
+	if err != nil {
+		return errors.Wrap(err, "generating deploy task")
+	}
+	tasks = append(tasks, taskDeploy)
+
+	// Generate pipeline from git resource and tasks
+	pipeline, err := generatePipeline(tasks)
+	if err != nil {
+		return errors.Wrap(err, "generating tekton pipeline")
 	}
 
-	// Create pipeline to tie together all tasks
-	pipeline := tekton.Pipeline{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pipeline",
-			APIVersion: "tekton.dev/v1alpha1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "skaffold-pipeline",
-		},
-		Spec: tekton.PipelineSpec{
-			Resources: []tekton.PipelineDeclaredResource{
-				{
-					Name: "source-repo",
-					Type: tekton.PipelineResourceTypeGit,
-				},
-			},
-			Tasks: []tekton.PipelineTask{},
-		},
-	}
-	//TODO: Ensure that tasks run in order
-	// Create tasks in pipeline spec for all corresponding tasks
-	for i, task := range tasks {
-		pipelineTask := tekton.PipelineTask{
-			Name: fmt.Sprintf("%s-task", task.Name),
-			TaskRef: tekton.TaskRef{
-				Name: task.Name,
-			},
-			RunAfter: []string{},
-			Resources: &tekton.PipelineTaskResources{
-				Inputs: []tekton.PipelineTaskInputResource{
-					{
-						Name:     "source",
-						Resource: "source-repo",
-					},
-				},
-			},
-		}
-		if i > 0 {
-			pipelineTask.RunAfter = []string{pipeline.Spec.Tasks[i-1].Name}
-		}
-		pipeline.Spec.Tasks = append(pipeline.Spec.Tasks, pipelineTask)
-	}
-
-	// json.Marshal all pieces of pipeline, then convert all to yaml and write them to file
+	// json.Marshal all pieces of pipeline, then convert all jsons to yamls
 	var jsons [][]byte
 	bGitResource, err := json.Marshal(gitResource)
 	if err != nil {
@@ -135,27 +103,33 @@ func (r *SkaffoldRunner) GeneratePipeline(ctx context.Context, out io.Writer, co
 	}
 	jsons = append(jsons, bPipeline)
 
+	var output bytes.Buffer
 	for _, item := range jsons {
 		itemYaml, err := yaml.JSONToYAML(item)
 		if err != nil {
-			return errors.Wrap(err, "converting json to yaml")
+			return errors.Wrap(err, "converting jsons to yamls")
 		}
 		output.Write(append(itemYaml, []byte("---\n")...))
 	}
+
+	// write all yaml pieces to output
 	if err := ioutil.WriteFile(fileOut, output.Bytes(), 0755); err != nil {
 		return err
 	}
-	return nil
 
+	return nil
 }
 
-func generateGitResource(config *latest.SkaffoldConfig) (tekton.PipelineResource, error) {
+func generateGitResource() (*tekton.PipelineResource, error) {
 	// Get git repo url
 	getGitRepo := exec.Command("git", "config", "--get", "remote.origin.url")
 	gitRepo, err := getGitRepo.Output()
+	if err != nil {
+		return nil, errors.Wrap(err, "getting git repo from git config")
+	}
 
 	// Create git resource for pipeline from users current git repo
-	return tekton.PipelineResource{
+	return &tekton.PipelineResource{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "PipelineResource",
 			APIVersion: "tekton.dev/v1alpha1",
@@ -172,14 +146,15 @@ func generateGitResource(config *latest.SkaffoldConfig) (tekton.PipelineResource
 				},
 			},
 		},
-	}, errors.Wrap(err, "getting git repo from git config")
+	}, nil
 }
 
-func generateBuildTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
-	if len(config.Pipeline.Build.Artifacts) == 0 {
-		return tekton.Task{}, errors.New("No artifacts to build")
+func generateBuildTask(buildConfig latest.BuildConfig) (*tekton.Task, error) {
+	if len(buildConfig.Artifacts) == 0 {
+		return nil, errors.New("No artifacts to build")
 	}
-	return tekton.Task{
+
+	return &tekton.Task{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Task",
 			APIVersion: "tekton.dev/v1alpha1",
@@ -213,13 +188,12 @@ func generateBuildTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
 	}, nil
 }
 
-func generateDeployTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
-	deployConfig := config.Pipeline.Deploy
+func generateDeployTask(deployConfig latest.DeployConfig) (*tekton.Task, error) {
 	if deployConfig.HelmDeploy == nil && deployConfig.KubectlDeploy == nil && deployConfig.KustomizeDeploy == nil {
-		return tekton.Task{}, errors.New("No Helm/Kubectl/Kustomize deploy config")
+		return nil, errors.New("No Helm/Kubectl/Kustomize deploy config")
 	}
 
-	return tekton.Task{
+	return &tekton.Task{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Task",
 			APIVersion: "tekton.dev/v1alpha1",
@@ -254,7 +228,57 @@ func generateDeployTask(config *latest.SkaffoldConfig) (tekton.Task, error) {
 	}, nil
 }
 
-func createSkaffoldProfile(config *latest.SkaffoldConfig, reader *bufio.Reader) error {
+func generatePipeline(tasks []*tekton.Task) (*tekton.Pipeline, error) {
+	if len(tasks) == 0 {
+		return nil, errors.New("no tasks to add to pipeline")
+	}
+
+	// Create pipeline to tie together all tasks
+	pipeline := &tekton.Pipeline{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pipeline",
+			APIVersion: "tekton.dev/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "skaffold-pipeline",
+		},
+		Spec: tekton.PipelineSpec{
+			Resources: []tekton.PipelineDeclaredResource{
+				{
+					Name: "source-repo",
+					Type: tekton.PipelineResourceTypeGit,
+				},
+			},
+			Tasks: []tekton.PipelineTask{},
+		},
+	}
+	// Create tasks in pipeline spec for all corresponding tasks
+	for i, task := range tasks {
+		pipelineTask := tekton.PipelineTask{
+			Name: fmt.Sprintf("%s-task", task.Name),
+			TaskRef: tekton.TaskRef{
+				Name: task.Name,
+			},
+			RunAfter: []string{},
+			Resources: &tekton.PipelineTaskResources{
+				Inputs: []tekton.PipelineTaskInputResource{
+					{
+						Name:     "source",
+						Resource: "source-repo",
+					},
+				},
+			},
+		}
+		if i > 0 {
+			pipelineTask.RunAfter = []string{pipeline.Spec.Tasks[i-1].Name}
+		}
+		pipeline.Spec.Tasks = append(pipeline.Spec.Tasks, pipelineTask)
+	}
+
+	return pipeline, nil
+}
+
+func createSkaffoldProfile(config *latest.SkaffoldConfig, fileOut string) error {
 	fmt.Println("Checking for oncluster skaffold profile...")
 	profileExists := false
 	for _, profile := range config.Profiles {
@@ -272,7 +296,7 @@ func createSkaffoldProfile(config *latest.SkaffoldConfig, reader *bufio.Reader) 
 	confirmLoop:
 		for {
 			fmt.Print("No profile \"oncluster\" found. Create one? [y/n]: ")
-			response, err := reader.ReadString('\n')
+			response, err := inputReader.ReadString('\n')
 			if err != nil {
 				return errors.Wrap(err, "reading user confirmation")
 			}
@@ -288,34 +312,12 @@ func createSkaffoldProfile(config *latest.SkaffoldConfig, reader *bufio.Reader) 
 	}
 
 	fmt.Println("Creating skaffold profile \"oncluster\"...")
-	newProfile := []latest.Profile{
-		{
-			Name: "oncluster",
-			Pipeline: latest.Pipeline{
-				Build:  config.Pipeline.Build,
-				Deploy: latest.DeployConfig{},
-			},
-		},
-	}
-	newProfile[0].Build.Cluster = &latest.ClusterDetails{
-		PullSecretName: "kaniko-secret",
-	}
-	newProfile[0].Build.LocalBuild = nil
-	// Add kaniko build config for artifacts
-	for _, artifact := range newProfile[0].Build.Artifacts {
-		artifact.ImageName = fmt.Sprintf("%s-pipeline", artifact.ImageName)
-		if artifact.DockerArtifact != nil {
-			fmt.Printf("Cannot use Docker to build %s on cluster. Adding config for building with Kaniko.\n", artifact.ImageName)
-			artifact.DockerArtifact = nil
-			artifact.KanikoArtifact = &latest.KanikoArtifact{
-				BuildContext: &latest.KanikoBuildContext{
-					GCSBucket: "skaffold-kaniko",
-				},
-			}
-		}
+	profile, err := generateProfile(config)
+	if err != nil {
+		return errors.Wrap(err, "generating profile \"oncluster\"")
 	}
 
-	bNewProfile, err := yamlv2.Marshal(newProfile)
+	bProfile, err := yamlv2.Marshal([]*latest.Profile{profile})
 	if err != nil {
 		return errors.Wrap(err, "marshaling new profile")
 	}
@@ -341,13 +343,42 @@ func createSkaffoldProfile(config *latest.SkaffoldConfig, reader *bufio.Reader) 
 
 	fileStrings = append(fileStrings, "")
 	copy(fileStrings[profilePos+1:], fileStrings[profilePos:])
-	fileStrings[profilePos] = strings.TrimSpace(string(bNewProfile))
+	fileStrings[profilePos] = strings.TrimSpace(string(bProfile))
 
 	fileContents = []byte((strings.Join(fileStrings, "\n")))
 
-	if err := ioutil.WriteFile("skaffold.yaml", fileContents, 0644); err != nil {
+	if err := ioutil.WriteFile(fileOut, fileContents, 0644); err != nil {
 		return errors.Wrap(err, "writing profile to skaffold.yaml")
 	}
 
 	return nil
+}
+
+func generateProfile(config *latest.SkaffoldConfig) (*latest.Profile, error) {
+	profile := &latest.Profile{
+		Name: "oncluster",
+		Pipeline: latest.Pipeline{
+			Build:  config.Pipeline.Build,
+			Deploy: latest.DeployConfig{},
+		},
+	}
+	profile.Build.Cluster = &latest.ClusterDetails{
+		PullSecretName: "kaniko-secret",
+	}
+	profile.Build.LocalBuild = nil
+	// Add kaniko build config for artifacts
+	for _, artifact := range profile.Build.Artifacts {
+		artifact.ImageName = fmt.Sprintf("%s-pipeline", artifact.ImageName)
+		if artifact.DockerArtifact != nil {
+			fmt.Printf("Cannot use Docker to build %s on cluster. Adding config for building with Kaniko.\n", artifact.ImageName)
+			artifact.DockerArtifact = nil
+			artifact.KanikoArtifact = &latest.KanikoArtifact{
+				BuildContext: &latest.KanikoBuildContext{
+					GCSBucket: "skaffold-kaniko",
+				},
+			}
+		}
+	}
+
+	return profile, nil
 }
--- pkg/skaffold/runner/generate_pipeline_test.go
+++ pkg/skaffold/runner/generate_pipeline_test.go
@@ -1,51 +1,164 @@
 package runner
 
 import (
-	"bytes"
 	"testing"
 
-	"github.com/GoogleContainerTools/skaffold/integration/examples/bazel/bazel-bazel/external/go_sdk/src/context"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
 	"github.com/GoogleContainerTools/skaffold/testutil"
+
+	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-//TODO(marlon-gamez): beef up testing
 func TestGeneratePipeline(t *testing.T) {
 	var tests = []struct {
-		description    string
-		testBench      *TestBench
-		skaffoldConfig *latest.SkaffoldConfig
-		shouldErr      bool
+		description string
+		tasks       []*tekton.Task
+		shouldErr   bool
 	}{
 		{
-			description: "successful pipeline generation",
-			testBench:   &TestBench{},
-			skaffoldConfig: &latest.SkaffoldConfig{
-				APIVersion: "skaffold/v1beta11",
-				Kind:       "Config",
+			description: "successful tekton pipeline generation",
+			tasks: []*tekton.Task{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-task",
+					},
+				},
 			},
 			shouldErr: false,
 		},
 		{
-			description:    "failed pipeline generation",
-			testBench:      &TestBench{},
-			skaffoldConfig: &latest.SkaffoldConfig{},
-			shouldErr:      true,
+			description: "fail generating tekton pipeline",
+			tasks:       []*tekton.Task{},
+			shouldErr:   true,
 		},
 	}
 
 	for _, test := range tests {
 		testutil.Run(t, test.description, func(t *testutil.T) {
-
-			// tempDir for writing file to
-			tempDir := t.NewTempDir()
-			filePath := tempDir.Root() + "/pipeline.yaml"
-
-			runner := createRunner(t, test.testBench, nil)
-			out := new(bytes.Buffer)
-			err := runner.GeneratePipeline(context.Background(), out, test.skaffoldConfig, filePath)
+			_, err := generatePipeline(test.tasks)
 
 			t.CheckError(test.shouldErr, err)
+		})
+	}
+}
+
+func TestGenerateBuildTask(t *testing.T) {
+	var tests = []struct {
+		description string
+		buildConfig latest.BuildConfig
+		shouldErr   bool
+	}{
+		{
+			description: "successfully generate build task",
+			buildConfig: latest.BuildConfig{
+				Artifacts: []*latest.Artifact{
+					{
+						ImageName: "testArtifact",
+					},
+				},
+			},
+			shouldErr: false,
+		},
+		{
+			description: "fail generating build task",
+			buildConfig: latest.BuildConfig{
+				Artifacts: []*latest.Artifact{},
+			},
+			shouldErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			_, err := generateBuildTask(test.buildConfig)
+			t.CheckError(test.shouldErr, err)
+		})
+	}
+}
+
+func TestGenerateDeployTask(t *testing.T) {
+	var tests = []struct {
+		description  string
+		deployConfig latest.DeployConfig
+		shouldErr    bool
+	}{
+		{
+			description: "successfully generate deploy task",
+			deployConfig: latest.DeployConfig{
+				DeployType: latest.DeployType{
+					HelmDeploy: &latest.HelmDeploy{},
+				},
+			},
+			shouldErr: false,
+		},
+		{
+			description: "fail generating deploy task",
+			deployConfig: latest.DeployConfig{
+				DeployType: latest.DeployType{
+					HelmDeploy:      nil,
+					KubectlDeploy:   nil,
+					KustomizeDeploy: nil,
+				},
+			},
+			shouldErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			_, err := generateDeployTask(test.deployConfig)
+			t.CheckError(test.shouldErr, err)
+		})
+	}
+}
+
+func TestGenerateProfile(t *testing.T) {
+	var tests = []struct {
+		description     string
+		skaffoldConfig  *latest.SkaffoldConfig
+		expectedProfile *latest.Profile
+		responses       []string
+		shouldErr       bool
+	}{
+		{
+			description: "successful profile generation",
+			skaffoldConfig: &latest.SkaffoldConfig{
+				Pipeline: latest.Pipeline{
+					Build: latest.BuildConfig{
+						Artifacts: []*latest.Artifact{
+							{
+								ImageName: "test",
+							},
+						},
+					},
+				},
+			},
+			expectedProfile: &latest.Profile{
+				Name: "oncluster",
+				Pipeline: latest.Pipeline{
+					Build: latest.BuildConfig{
+						Artifacts: []*latest.Artifact{
+							{
+								ImageName: "test-pipeline",
+							},
+						},
+						BuildType: latest.BuildType{
+							Cluster: &latest.ClusterDetails{
+								PullSecretName: "kaniko-secret",
+							},
+						},
+					},
+				},
+			},
+			shouldErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			profile, err := generateProfile(test.skaffoldConfig)
+			t.CheckErrorAndDeepEqual(test.shouldErr, err, test.expectedProfile, profile)
 		})
 	}
 }
