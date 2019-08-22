4be9184c53b9643b3ace6a7bf0c384d1ed09e861
Cedric Kring <cedric.kring@gmail.com>
2019-08-22 20:40:25 +0200

    Fix tests

--- pkg/skaffold/schema/profiles_test.go
+++ pkg/skaffold/schema/profiles_test.go
@@ -128,6 +128,7 @@ func TestApplyProfiles(t *testing.T) {
 									DockerImage: "gcr.io/cloud-builders/docker",
 									MavenImage:  "gcr.io/cloud-builders/mvn",
 									GradleImage: "gcr.io/cloud-builders/gradle",
+									KanikoImage: "gcr.io/kaniko-project/executor",
 								},
 							},
 						},
