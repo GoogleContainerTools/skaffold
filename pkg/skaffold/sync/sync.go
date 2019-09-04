3fd5dd126727473ce940495d326534e328566015
David Gageot <david@gageot.net>
2019-09-04 17:47:29 +0200

    Remove misleading log (#2802)
    Fix #2797
    
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/sync/sync.go
+++ pkg/skaffold/sync/sync.go
@@ -234,9 +234,7 @@ func Perform(ctx context.Context, image string, files syncMap, cmdFn func(contex
 		}
 
 		for _, p := range pods.Items {
-
 			if p.Status.Phase != v1.PodRunning {
-				logrus.Infof("Skipping sync with pod %s because it's not running", p.Name)
 				continue
 			}
 
