89811d243e0810e91e037646591131917b3eb216
Tejal Desai <tejal29@gmail.com>
2019-09-09 16:11:01 -0700

    spell check

--- pkg/skaffold/deploy/status_check.go
+++ pkg/skaffold/deploy/status_check.go
@@ -58,7 +58,7 @@ type counter struct {
 func StatusCheck(ctx context.Context, defaultLabeller *DefaultLabeller, runCtx *runcontext.RunContext, out io.Writer) error {
 	client, err := pkgkubernetes.Client()
 	if err != nil {
-		return errors.Wrap(err, "getting kubernetes cli")
+		return errors.Wrap(err, "getting kubernetes client")
 	}
 
 	deadline := getDeadline(runCtx.Cfg.Deploy.StatusCheckDeadlineSeconds)
