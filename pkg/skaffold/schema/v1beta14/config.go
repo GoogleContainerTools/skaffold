333167c4ea2d51d708e8f2fa7aa55b0361130148
Tejal Desai <tejaldesai@google.com>
2019-09-13 13:58:36 -0700

    create new v1beta15 config

--- pkg/skaffold/schema/v1beta14/config.go
+++ pkg/skaffold/schema/v1beta14/config.go
@@ -14,13 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package latest
+package v1beta14
 
 import (
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/util"
 )
 
-const Version string = "skaffold/v1beta15"
+const Version string = "skaffold/v1beta14"
 
 // NewSkaffoldConfig creates a SkaffoldConfig
 func NewSkaffoldConfig() util.VersionedConfig {
