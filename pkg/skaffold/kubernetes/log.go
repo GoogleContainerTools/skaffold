eea87c16b79d3cdcc1ad06526820fa88e6969df7
Balint Pato <balopat@users.noreply.github.com>
2019-10-14 13:28:11 -0700

    remove Container Was Terminated message (#3054)

--- pkg/skaffold/kubernetes/log.go
+++ pkg/skaffold/kubernetes/log.go
@@ -194,10 +194,6 @@ func (a *LogAggregator) streamRequest(ctx context.Context, headerColor color.Col
 			// Read up to newline
 			line, err := r.ReadString('\n')
 			if err == io.EOF {
-				// Unless the context was interrupted, this means that the container was stopped.
-				if ctx.Err() != context.Canceled {
-					a.printLogLine(headerColor, prefix, "<Container was Terminated>\n")
-				}
 				return nil
 			}
 			if err != nil {
