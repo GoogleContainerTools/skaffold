9bd39a8de9ab8559768923095beaa40f55453353
David Gageot <david@gageot.net>
2019-11-14 07:59:21 +0100

    Support Buildpacks on GCB
    Signed-off-by: David Gageot <david@gageot.net>

--- docs/content/en/docs/design/_index.md
+++ docs/content/en/docs/design/_index.md
@@ -15,8 +15,8 @@ provides built-in support for the following tools:
   * Dockerfile locally, in-cluster with kaniko or on cloud using Google Cloud Build
   * Jib Maven and Jib Gradle locally or on cloud using Google Cloud Build
   * Bazel locally
-  * Cloud Native Buildpacks locally
-  * Custom script locally
+  * Cloud Native Buildpacks locally or on cloud using Google Cloud Build
+  * Custom script locally or in-cluster
 * **Test**
   * [container-structure-test](https://github.com/GoogleContainerTools/container-structure-test)
 * **Tag**
---
+++ pkg/skaffold/build/gcb/buildpacks.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gcb
+
+import (
+	cloudbuild "google.golang.org/api/cloudbuild/v1"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest"
+)
+
+func (b *Builder) buildpackBuildSpec(artifact *latest.BuildpackArtifact, tag string) (cloudbuild.Build, error) {
+	steps := []*cloudbuild.BuildStep{
+		{
+			Name: "busybox",
+			Args: []string{"sh", "-c", "chown -R 1000:1000 /workspace /layers $$HOME"},
+		},
+		{
+			// TODO: env
+			Name:       artifact.Builder,
+			Entrypoint: "/lifecycle/detector",
+		},
+		{
+			Name:       artifact.Builder,
+			Entrypoint: "/lifecycle/analyzer",
+			Args:       []string{tag},
+		},
+		{
+			// TODO: env
+			Name:       artifact.Builder,
+			Entrypoint: "/lifecycle/builder",
+		},
+	}
+
+	if artifact.RunImage == "" {
+		steps = append(steps,
+			&cloudbuild.BuildStep{
+				Name:       artifact.Builder,
+				Entrypoint: "/lifecycle/exporter",
+				Args:       []string{tag},
+			},
+		)
+	} else {
+		steps = append(steps,
+			&cloudbuild.BuildStep{
+				Name: b.DockerImage,
+				Args: []string{"pull", artifact.RunImage},
+			},
+			&cloudbuild.BuildStep{
+				Name:       artifact.Builder,
+				Entrypoint: "/lifecycle/exporter",
+				Args:       []string{"-image", artifact.RunImage, tag},
+			},
+		)
+	}
+
+	return cloudbuild.Build{
+		Options: &cloudbuild.BuildOptions{
+			Volumes: []*cloudbuild.Volume{{Name: "layers", Path: "/layers"}},
+		},
+		Steps: steps,
+	}, nil
+}
