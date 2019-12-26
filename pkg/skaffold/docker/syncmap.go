e717446dce46b010b772b4f28e24445736d030c7
David Gageot <david@gageot.net>
2019-12-26 07:48:13 +0100

    SyncMap is a matter of artifact type, not builder
    Fixes #3448

--- pkg/skaffold/docker/syncmap.go
+++ pkg/skaffold/docker/syncmap.go
@@ -17,7 +17,6 @@ limitations under the License.
 package docker
 
 import (
-	"context"
 	"os"
 	"path"
 	"path/filepath"
@@ -30,7 +29,7 @@ import (
 // SyncMap creates a map of syncable files by looking at the COPY/ADD commands in the Dockerfile.
 // All keys are relative to the Skaffold root, the destinations are absolute container paths.
 // TODO(corneliusweig) destinations are not resolved across stages in multistage dockerfiles. Is there a use-case for that?
-func SyncMap(ctx context.Context, workspace string, dockerfilePath string, buildArgs map[string]*string, insecureRegistries map[string]bool) (map[string][]string, error) {
+func SyncMap(workspace string, dockerfilePath string, buildArgs map[string]*string, insecureRegistries map[string]bool) (map[string][]string, error) {
 	absDockerfilePath, err := NormalizeDockerfilePath(workspace, dockerfilePath)
 	if err != nil {
 		return nil, errors.Wrap(err, "normalizing dockerfile path")
