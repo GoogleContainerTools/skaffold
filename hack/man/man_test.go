4f8c29be2ff8b81f65511ed73d5c0010a65631ab
David Gageot <david@gageot.net>
2019-12-31 11:40:15 +0100

    Move man generation to hack folder
    Signed-off-by: David Gageot <david@gageot.net>

--- hack/man/man_test.go
+++ hack/man/man_test.go
@@ -40,11 +40,11 @@ func TestPrintMan(t *testing.T) {
 	testutil.CheckContains(t, "Env vars", output)
 
 	// Compare to current man page
-	header, err := ioutil.ReadFile(filepath.Join("..", "..", "..", "docs", "content", "en", "docs", "references", "cli", "index_header"))
+	header, err := ioutil.ReadFile(filepath.Join("..", "..", "docs", "content", "en", "docs", "references", "cli", "index_header"))
 	testutil.CheckError(t, false, err)
 	header = bytes.Replace(header, []byte("\r\n"), []byte("\n"), -1)
 
-	expected, err := ioutil.ReadFile(filepath.Join("..", "..", "..", "docs", "content", "en", "docs", "references", "cli", "_index.md"))
+	expected, err := ioutil.ReadFile(filepath.Join("..", "..", "docs", "content", "en", "docs", "references", "cli", "_index.md"))
 	testutil.CheckError(t, false, err)
 	expected = bytes.Replace(expected, []byte("\r\n"), []byte("\n"), -1)
 
