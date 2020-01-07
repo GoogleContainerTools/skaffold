defda6aa9246c2bf57a755c821edc6070101f643
Appu Goundan <appu@google.com>
2020-01-07 17:56:40 -0500

    Add common jib sync code for processing syncmap
    Converts json output from jib maven/gradle command to a syncmap
    that can be processed within skaffold

---
+++ pkg/skaffold/build/jib/sync.go
@@ -0,0 +1,94 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jib
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"os/exec"
+	"regexp"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
+)
+
+type SyncMap map[string]SyncEntry
+
+type SyncEntry struct {
+	Dest     []string
+	FileTime time.Time
+	IsDirect bool
+}
+
+type JSONSyncMap struct {
+	Direct    []JSONSyncEntry `json:"direct"`
+	Generated []JSONSyncEntry `json:"generated"`
+}
+
+type JSONSyncEntry struct {
+	Src  string `json:"src"`
+	Dest string `json:"dest"`
+}
+
+func getSyncMapFromSystem(cmd *exec.Cmd) (*SyncMap, error) {
+	jsm := JSONSyncMap{}
+	stdout, err := util.RunCmdOut(cmd)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get Jib sync map")
+	}
+
+	// To parse the output, search for "BEGIN JIB JSON", then unmarshal the next line into the pathMap struct.
+	// Syncmap is transitioning to "BEGIN JIB JSON: SYNCMAP/1" starting in jib 2.0.0
+	// perhaps this feature should only be included from 2.0.0 onwards? And we generally avoid this?
+	matches := regexp.MustCompile(`BEGIN JIB JSON(?:: SYNCMAP/1)?\r?\n({.*})`).FindSubmatch(stdout)
+	if len(matches) == 0 {
+		return nil, errors.New("failed to get Jib Sync data")
+	}
+
+	line := bytes.Replace(matches[1], []byte(`\`), []byte(`\\`), -1)
+	if err := json.Unmarshal(line, &jsm); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	sm := make(SyncMap)
+	for _, de := range jsm.Direct {
+		info, err := os.Stat(de.Src)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not obtain file mod time")
+		}
+		sm[de.Src] = SyncEntry{
+			Dest:     []string{de.Dest},
+			FileTime: info.ModTime(),
+			IsDirect: true,
+		}
+	}
+	for _, ge := range jsm.Generated {
+		info, err := os.Stat(ge.Src)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not obtain file mod time")
+		}
+		sm[ge.Src] = SyncEntry{
+			Dest:     []string{ge.Dest},
+			FileTime: info.ModTime(),
+			IsDirect: false,
+		}
+	}
+	return &sm, nil
+}
---
+++ pkg/skaffold/build/jib/sync_test.go
@@ -0,0 +1,134 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jib
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
+	"github.com/GoogleContainerTools/skaffold/testutil"
+)
+
+func TestGetSyncMapFromSystem(t *testing.T) {
+	tmpDir, cleanup := testutil.NewTempDir(t)
+	defer cleanup()
+
+	tmpDir.Touch("dep1", "dir/dep2")
+	dep1 := tmpDir.Path("dep1")
+	dep2 := tmpDir.Path("dir/dep2")
+
+	dep1Time := getFileTime(dep1, t)
+	dep2Time := getFileTime(dep2, t)
+
+	dep1Target := "/target/dep1"
+	dep2Target := "/target/anotherDir/dep2"
+
+	tests := []struct {
+		description string
+		stdout      string
+		shouldErr   bool
+		expected    *SyncMap
+	}{
+		{
+			description: "empty",
+			stdout:      "",
+			shouldErr:   true,
+			expected:    nil,
+		},
+		{
+			description: "old style marker",
+			stdout:      "BEGIN JIB JSON\n{}",
+			shouldErr:   false,
+			expected:    &SyncMap{},
+		},
+		{
+			description: "bad marker",
+			stdout:      "BEGIN JIB JSON: BAD/1\n{}",
+			shouldErr:   true,
+			expected:    nil,
+		},
+		{
+			description: "direct only",
+			stdout: "BEGIN JIB JSON: SYNCMAP/1\n" +
+				fmt.Sprintf("{\"direct\":[{\"src\":\"%s\",\"dest\":\"%s\"}]}", dep1, dep1Target),
+			shouldErr: false,
+			expected: &SyncMap{
+				dep1: SyncEntry{
+					[]string{dep1Target},
+					dep1Time,
+					true,
+				},
+			},
+		},
+		{
+			description: "generated only",
+			stdout: "BEGIN JIB JSON: SYNCMAP/1\n" +
+				fmt.Sprintf("{\"generated\":[{\"src\":\"%s\",\"dest\":\"%s\"}]}", dep1, dep1Target),
+			shouldErr: false,
+			expected: &SyncMap{
+				dep1: SyncEntry{
+					[]string{dep1Target},
+					dep1Time,
+					false,
+				},
+			},
+		},
+		{
+			description: "generated and direct",
+			stdout: "BEGIN JIB JSON: SYNCMAP/1\n" +
+				fmt.Sprintf("{\"direct\":[{\"src\":\"%s\",\"dest\":\"%s\"}],\"generated\":[{\"src\":\"%s\",\"dest\":\"%s\"}]}", dep1, dep1Target, dep2, dep2Target),
+			shouldErr: false,
+			expected: &SyncMap{
+				dep1: SyncEntry{
+					[]string{dep1Target},
+					dep1Time,
+					true,
+				},
+				dep2: SyncEntry{
+					Dest:     []string{dep2Target},
+					FileTime: dep2Time,
+					IsDirect: false,
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			t.Override(&util.DefaultExecCommand, testutil.CmdRunOut(
+				"ignored",
+				test.stdout,
+			))
+
+			results, err := getSyncMapFromSystem(&exec.Cmd{Args: []string{"ignored"}})
+
+			t.CheckErrorAndDeepEqual(test.shouldErr, err, test.expected, results)
+		})
+	}
+}
+
+func getFileTime(file string, t *testing.T) time.Time {
+	info, err := os.Stat(file)
+	if err != nil {
+		t.Fatalf("Failed to stat %s", file)
+		return time.Time{}
+	}
+	return info.ModTime()
+}
