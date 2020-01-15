1ac3b2a36d58017f818e3b020f700eeef6e63944
Farhad Vildanov <spectrox25@gmail.com>
2020-01-15 11:19:22 +0300

    Dockerfile detector will only check files containing "Dockerfile" in the name (#3499)
    * Dockerfile detector will only check files containing "Dockerfile" in the name.
    
    Fixes #3484.
    
    * Make filename check case-insensitive.

--- pkg/skaffold/initializer/init_test.go
+++ pkg/skaffold/initializer/init_test.go
@@ -142,14 +142,17 @@ func TestWalk(t *testing.T) {
 		{
 			description: "should return correct k8 configs and build files (backwards compatibility)",
 			filesWithContents: map[string]string{
-				"config/test.yaml":    validK8sManifest,
-				"config/invalid.yaml": emptyFile,
-				"k8pod.yml":           validK8sManifest,
-				"README":              emptyFile,
-				"deploy/Dockerfile":   emptyFile,
-				"gradle/build.gradle": emptyFile,
-				"maven/pom.xml":       emptyFile,
-				"Dockerfile":          emptyFile,
+				"config/test.yaml":       validK8sManifest,
+				"config/invalid.yaml":    emptyFile,
+				"k8pod.yml":              validK8sManifest,
+				"README":                 emptyFile,
+				"deploy/Dockerfile":      emptyFile,
+				"deploy/Dockerfile.dev":  emptyFile,
+				"deploy/dev.Dockerfile":  emptyFile,
+				"deploy/test.dockerfile": emptyFile,
+				"gradle/build.gradle":    emptyFile,
+				"maven/pom.xml":          emptyFile,
+				"Dockerfile":             emptyFile,
 			},
 			force: false,
 			expectedConfigs: []string{
@@ -159,6 +162,9 @@ func TestWalk(t *testing.T) {
 			expectedPaths: []string{
 				"Dockerfile",
 				"deploy/Dockerfile",
+				"deploy/Dockerfile.dev",
+				"deploy/dev.Dockerfile",
+				"deploy/test.dockerfile",
 			},
 			shouldErr: false,
 		},
@@ -342,7 +348,7 @@ deploy:
 }
 
 func fakeValidateDockerfile(path string) bool {
-	return strings.HasSuffix(path, "Dockerfile")
+	return strings.Contains(strings.ToLower(path), "dockerfile")
 }
 
 func fakeValidateJibConfig(path string) []jib.ArtifactConfig {
