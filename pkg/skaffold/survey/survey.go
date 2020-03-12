c00066202b01ecfe50197ec4efa6f08702e10c70
Tejal Desai <tejal29@gmail.com>
2020-03-12 15:05:22 -0700

    Update pkg/skaffold/survey/survey.go
    Co-Authored-By: Nick Kubala <nkubala@users.noreply.github.com>

--- pkg/skaffold/survey/survey.go
+++ pkg/skaffold/survey/survey.go
@@ -26,7 +26,7 @@ const (
 	Prompt = `Help improve Skaffold! Take a 10 seconds anonymous survey by running
    $skaffold survey`
 
-	Form = `Thank you for offering your feedback on Skaffold. Understanding your experiences and opinions helps us make skaffold better for you and other users
+	Form = `Thank you for offering your feedback on Skaffold! Understanding your experiences and opinions helps us make Skaffold better for you and other users.
     Please take survey at https://forms.gle/BMTbGQXLWSdn7vEs6
 
 To permanently disable the survey prompt, run:
