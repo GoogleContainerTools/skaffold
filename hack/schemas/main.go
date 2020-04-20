cf9f2cf22885ef9907aea50ce96ecca61698cf9b
David Gageot <david@gageot.net>
2020-04-20 20:53:35 +0200

    ResourceType is of type string (#3987)
    * ResourceType is of type string
    
    Fix #3623
    
    Signed-off-by: David Gageot <david@gageot.net>
    
    * Ignore the linter
    
    The linter wants us to create a “string” const
    
    Signed-off-by: David Gageot <david@gageot.net>

--- hack/schemas/main.go
+++ hack/schemas/main.go
@@ -178,10 +178,13 @@ func yamlFieldName(field *ast.Field) string {
 	return strings.Split(yamlTag, ",")[0]
 }
 
+//nolint:golint,goconst
 func setTypeOrRef(def *Definition, typeName string) {
 	switch typeName {
-	case "string":
-		def.Type = typeName
+	// Special case for ResourceType that is an alias of string.
+	// Fixes #3623
+	case "string", "ResourceType":
+		def.Type = "string"
 	case "bool":
 		def.Type = "boolean"
 	case "int", "int64", "int32":
