b2bf1d550edc98222cae3194033331aa4b96c083
David Gageot <david@gageot.net>
2020-04-30 11:24:28 +0200

    Change default buildpacks
    Signed-off-by: David Gageot <david@gageot.net>

--- cmd/skaffold/app/cmd/init.go
+++ cmd/skaffold/app/cmd/init.go
@@ -70,7 +70,7 @@ func NewCmdInit() *cobra.Command {
 			f.MarkHidden("XXenableJibGradleInit")
 			f.BoolVar(&enableBuildpacksInit, "XXenableBuildpacksInit", false, "")
 			f.MarkHidden("XXenableBuildpacksInit")
-			f.StringVar(&buildpacksBuilder, "XXdefaultBuildpacksBuilder", "heroku/buildpacks", "")
+			f.StringVar(&buildpacksBuilder, "XXdefaultBuildpacksBuilder", "gcr.io/buildpacks/builder", "")
 			f.MarkHidden("XXdefaultBuildpacksBuilder")
 			f.BoolVar(&enableManifestGeneration, "XXenableManifestGeneration", false, "")
 			f.MarkHidden("XXenableManifestGeneration")
