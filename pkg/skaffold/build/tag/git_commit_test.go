d82eb3c3cdba5e2ebb9c53a851f8a67c44704067
David Gageot <david@gageot.net>
2020-04-30 09:50:32 +0200

    Upgrade go-git to v5 (#4051)
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/build/tag/git_commit_test.go
+++ pkg/skaffold/build/tag/git_commit_test.go
@@ -26,9 +26,9 @@ import (
 	"testing"
 	"time"
 
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 
 	"github.com/GoogleContainerTools/skaffold/testutil"
 )
