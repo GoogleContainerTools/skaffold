594acfa8958cbe911505e083176582057abec810
David Gageot <david@gageot.net>
2020-05-11 08:16:51 +0200

    Running the linter on GitHub Actions
    Signed-off-by: David Gageot <david@gageot.net>

--- hack/linters.sh
+++ hack/linters.sh
@@ -18,13 +18,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-echo "Running validation scripts..."
+echo "Running linters..."
 scripts=(
-    "hack/check-schema-changes.sh"
-    "hack/check-skaffold-builder.sh"
-    "hack/check-samples.sh"
-    "hack/check-docs.sh"
-    "hack/test-generated-proto.sh"
+    "hack/boilerplate.sh"
+    "hack/gofmt.sh"
+    "hack/pedantic-imports.sh"
+    "hack/golangci-lint.sh"
 )
 fail=0
 for s in "${scripts[@]}"; do
