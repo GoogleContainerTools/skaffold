04fa5e940003d63f2e0c940fb850b10ef04a8557
David Gageot <david@gageot.net>
2020-07-11 20:59:44 +0200

    When pulling images and authentication fails, first try anonymous pulling. (#4451)
    Fixes #4447
    
    Signed-off-by: David Gageot <david@gageot.net>

--- pkg/skaffold/build/buildpacks/lifecycle.go
+++ pkg/skaffold/build/buildpacks/lifecycle.go
@@ -124,6 +124,7 @@ func runPackBuild(ctx context.Context, out io.Writer, localDocker docker.LocalDa
 	packClient, err := pack.NewClient(
 		pack.WithDockerClient(localDocker.RawClient()),
 		pack.WithLogger(NewLogger(out)),
+		pack.WithFetcher(newFetcher(out, localDocker)),
 	)
 	if err != nil {
 		return fmt.Errorf("unable to create pack client: %w", err)
