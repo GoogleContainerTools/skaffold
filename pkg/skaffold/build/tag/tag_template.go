57082cecf099cbee9a9191b531c776c8ea1dff69
Felix <felixtran39>
2020-07-24 15:39:06 -0400

    tagTemplate tagger code logic

--- pkg/skaffold/build/tag/tag_template.go
+++ pkg/skaffold/build/tag/tag_template.go
@@ -18,11 +18,71 @@ package tag
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"text/template"
 
 	"github.com/sirupsen/logrus"
 )
+
+// tagTemplateTagger implements Tagger
+type tagTemplateTagger struct {
+	Template   *template.Template
+	Components map[string]Tagger
+}
+
+// NewTagTemplateTagger creates a new tagTemplateTagger
+func NewTagTemplateTagger(t string, components map[string]Tagger) (Tagger, error) {
+	tmpl, err := ParseTagTemplate(t)
+	if err != nil {
+		return nil, fmt.Errorf("parsing template: %w", err)
+	}
+
+	return &tagTemplateTagger{
+		Template:   tmpl,
+		Components: components,
+	}, nil
+}
+
+// GenerateTag generates a tag from a template referencing tagging strategies.
+func (t *tagTemplateTagger) GenerateTag(workingDir, imageName string) (string, error) {
+	customMap, err := t.EvaluateComponents(workingDir, imageName)
+	if err != nil {
+		return "", err
+	}
+
+	tag, err := ExecuteTagTemplate(t.Template.Option("missingkey=error"), customMap)
+	if err != nil {
+		return "", err
+	}
+
+	return tag, nil
+}
+
+// EvaluateComponents creates a custom mapping of component names to their tagger string representation.
+func (t *tagTemplateTagger) EvaluateComponents(workingDir, imageName string) (map[string]string, error) {
+	customMap := map[string]string{}
+
+	gitTagger, _ := NewGitCommit("", "")
+	dateTimeTagger := NewDateTimeTagger("", "")
+
+	for k, v := range map[string]Tagger{"GIT": gitTagger, "DATE": dateTimeTagger, "SHA": &ChecksumTagger{}} {
+		tag, _ := v.GenerateTag(workingDir, imageName)
+		customMap[k] = tag
+	}
+
+	for k, v := range t.Components {
+		if _, ok := v.(*tagTemplateTagger); ok {
+			return nil, errors.New("invalid component tagTemplate")
+		}
+		tag, err := v.GenerateTag(workingDir, imageName)
+		if err != nil {
+			return nil, fmt.Errorf("evaluating component: %w", err)
+		}
+		customMap[k] = tag
+	}
+	return customMap, nil
+}
 
 // ParseTagTemplate is a simple wrapper to parse an tag template.
 func ParseTagTemplate(t string) (*template.Template, error) {
