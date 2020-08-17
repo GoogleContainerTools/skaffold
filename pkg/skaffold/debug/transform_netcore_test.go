cc79d19e19fee27606d5f6b544c623098f8ad441
MrLuje <ogmniluje@gmail.com>
2020-08-17 08:24:53 +0200

    add tests for netcore debugging

---
+++ pkg/skaffold/debug/transform_netcore_test.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package debug
+
+import (
+	"testing"
+
+	"github.com/GoogleContainerTools/skaffold/testutil"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestNetcoreTransformer_IsApplicable(t *testing.T) {
+	tests := []struct {
+		description string
+		source      imageConfiguration
+		launcher    string
+		result      bool
+	}{
+		{
+			description: "entrypoint with dotnet",
+			source:      imageConfiguration{entrypoint: []string{"dotnet", "myapp.dll"}},
+			result:      true,
+		},
+		{
+			description: "entrypoint /bin/sh",
+			source:      imageConfiguration{entrypoint: []string{"/bin/sh"}},
+			result:      false,
+		},
+		{
+			description: "nothing",
+			source:      imageConfiguration{},
+			result:      false,
+		},
+	}
+
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			t.Override(&entrypointLaunchers, []string{test.launcher})
+			result := netcoreTransformer{}.IsApplicable(test.source)
+
+			t.CheckDeepEqual(test.result, result)
+		})
+	}
+}
+
+func TestNetcoreTransformerApply(t *testing.T) {
+	tests := []struct {
+		description   string
+		containerSpec v1.Container
+		configuration imageConfiguration
+		shouldErr     bool
+		result        v1.Container
+		debugConfig   ContainerDebugConfiguration
+		image         string
+	}{
+		{
+			description:   "empty",
+			containerSpec: v1.Container{},
+			configuration: imageConfiguration{},
+			debugConfig:   ContainerDebugConfiguration{Runtime: "netcore"},
+			image:         "netcore",
+			shouldErr:     false,
+		},
+		{
+			description:   "basic",
+			containerSpec: v1.Container{},
+			configuration: imageConfiguration{entrypoint: []string{"dotnet", "myapp.dll"}},
+			result:        v1.Container{},
+			debugConfig:   ContainerDebugConfiguration{Runtime: "netcore"},
+			image:         "netcore",
+			shouldErr:     false,
+		},
+	}
+	var identity portAllocator = func(port int32) int32 {
+		return port
+	}
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			config, image, err := netcoreTransformer{}.Apply(&test.containerSpec, test.configuration, identity)
+
+			t.CheckError(test.shouldErr, err)
+			t.CheckDeepEqual(test.result, test.containerSpec)
+			t.CheckDeepEqual(test.debugConfig, config)
+			t.CheckDeepEqual(test.image, image)
+		})
+	}
+}
