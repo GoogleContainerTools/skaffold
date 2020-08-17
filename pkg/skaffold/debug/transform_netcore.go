b50bdde5f49e6311c859caa2da83cfafe77bba6b
MrLuje <ogmniluje@gmail.com>
2020-08-17 08:24:36 +0200

    basic support of netcore debugging

---
+++ pkg/skaffold/debug/transform_netcore.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2019 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package debug
+
+import (
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
+)
+
+type netcoreTransformer struct{}
+
+func init() {
+	containerTransforms = append(containerTransforms, netcoreTransformer{})
+}
+
+// isLaunchingNetcore determines if the arguments seems to be invoking dotnet
+func isLaunchingNetcore(args []string) bool {
+	return len(args) > 0 && (args[0] == "dotnet" || strings.HasSuffix(args[0], "/dotnet"))
+}
+
+func (t netcoreTransformer) IsApplicable(config imageConfiguration) bool {
+	if len(config.entrypoint) > 0 && !isEntrypointLauncher(config.entrypoint) {
+		return isLaunchingNetcore(config.entrypoint)
+	}
+	return false
+}
+
+// Apply configures a container definition for vsdbg.
+// Returns a simple map describing the debug configuration details.
+func (t netcoreTransformer) Apply(container *v1.Container, config imageConfiguration, portAlloc portAllocator) (ContainerDebugConfiguration, string, error) {
+	logrus.Infof("Configuring %q for netcore debugging", container.Name)
+
+	return ContainerDebugConfiguration{
+		Runtime: "netcore",
+	}, "netcore", nil
+}
