6a108ee535479ee6e02829a410da686d5f2b347e
Priya Modali <modali@google.com>
2020-10-26 20:12:00 -0700

    Updated the known problens with deploy errors. Updated the deploy error fromat.

--- pkg/skaffold/errors/errors.go
+++ pkg/skaffold/errors/errors.go
@@ -72,7 +72,8 @@ func ActionableErr(phase Phase, err error) *proto.ActionableErr {
 }
 
 func ShowAIError(err error) error {
-	for _, v := range append(knownBuildProblems, knownInitProblems...) {
+	var knownProblems = append(knownBuildProblems, knownDeployProblems...)
+	for _, v := range append(knownProblems, knownInitProblems...) {
 		if v.regexp.MatchString(err.Error()) {
 			if suggestions := v.suggestion(skaffoldOpts); suggestions != nil {
 				description := fmt.Sprintf("%s\n", err)
