fd73a77fc099517bf4e5369fe92549b6d305a154
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2020-12-09 17:07:02 +0400

    feat: update moby/buildkit to v0.8.0 and re-enable instructions.Parse

--- go.mod
+++ go.mod
@@ -51,7 +51,7 @@ require (
 	github.com/mattn/go-colorable v0.1.8
 	github.com/mitchellh/go-homedir v1.1.0
 	// github.com/moby/buildkit v0.7.1
-	github.com/moby/buildkit v0.3.3
+	github.com/moby/buildkit v0.8.0
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.1
 	github.com/opencontainers/runc v1.0.0-rc92 // indirect
@@ -65,7 +65,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/tektoncd/pipeline v0.5.1-0.20190731183258-9d7e37e85bf8
 	github.com/xeipuuv/gojsonschema v1.2.0
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
 	golang.org/x/mod v0.3.0
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
--- pkg/skaffold/docker/parse.go
+++ pkg/skaffold/docker/parse.go
@@ -27,6 +27,7 @@ import (
 
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/moby/buildkit/frontend/dockerfile/command"
+	"github.com/moby/buildkit/frontend/dockerfile/instructions"
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
 	"github.com/moby/buildkit/frontend/dockerfile/shell"
 	"github.com/sirupsen/logrus"
@@ -76,12 +77,10 @@ func readCopyCmdsFromDockerfile(onlyLastImage bool, absDockerfilePath, workspace
 		return nil, fmt.Errorf("parsing dockerfile %q: %w", absDockerfilePath, err)
 	}
 
-	// TODO: re-enable this after updating moby/buildkit dependency
-	// https://github.com/GoogleContainerTools/skaffold/issues/5049
 	// instructions.Parse will check for malformed Dockerfile
-	// if _, _, err := instructions.Parse(res.AST); err != nil {
-	// 	return nil, fmt.Errorf("parsing dockerfile %q: %w", absDockerfilePath, err)
-	// }
+	if _, _, err := instructions.Parse(res.AST); err != nil {
+		return nil, fmt.Errorf("parsing dockerfile %q: %w", absDockerfilePath, err)
+	}
 
 	dockerfileLines := res.AST.Children
 
