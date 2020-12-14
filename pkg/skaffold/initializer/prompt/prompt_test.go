ff0d65cb311e416b9f77ef52a7fdd1dc47686dc3
Marlon Gamez <marlongamez98@gmail.com>
2020-12-14 15:46:17 -0800

    replace our user input handling with survey package (#5142)
    * replace our own input handling with survey package
    
    * replace our own input handling with survey package
    
    * add unit tests for affected prompt functions

--- pkg/skaffold/initializer/prompt/prompt_test.go
+++ pkg/skaffold/initializer/prompt/prompt_test.go
@@ -27,6 +27,102 @@ import (
 	"github.com/GoogleContainerTools/skaffold/testutil"
 )
 
+func TestWriteSkaffoldConfig(t *testing.T) {
+	tests := []struct {
+		description    string
+		config         *latest.SkaffoldConfig
+		promptResponse bool
+		expectedDone   bool
+		shouldErr      bool
+	}{
+		{
+			description:    "yes response",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: true,
+			expectedDone:   false,
+			shouldErr:      false,
+		},
+		{
+			description:    "no response",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: false,
+			expectedDone:   true,
+			shouldErr:      false,
+		},
+		{
+			description:    "error",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: false,
+			expectedDone:   true,
+			shouldErr:      true,
+		},
+	}
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			t.Override(&askOne, func(_ survey.Prompt, response interface{}, _ ...survey.AskOpt) error {
+				r := response.(*bool)
+				*r = test.promptResponse
+
+				if test.shouldErr {
+					return errors.New("error")
+				}
+				return nil
+			})
+
+			done, err := WriteSkaffoldConfig(ioutil.Discard, []byte{}, nil, "")
+			t.CheckErrorAndDeepEqual(test.shouldErr, err, test.expectedDone, done)
+		})
+	}
+}
+
+func TestPortForwardResource(t *testing.T) {
+	tests := []struct {
+		description    string
+		config         *latest.SkaffoldConfig
+		promptResponse string
+		expected       int
+		shouldErr      bool
+	}{
+		{
+			description:    "valid response",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: "8080",
+			expected:       8080,
+			shouldErr:      false,
+		},
+		{
+			description:    "empty response",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: "",
+			expected:       0,
+			shouldErr:      false,
+		},
+		{
+			description:    "error",
+			config:         &latest.SkaffoldConfig{},
+			promptResponse: "",
+			expected:       0,
+			shouldErr:      true,
+		},
+	}
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			t.Override(&ask, func(_ []*survey.Question, response interface{}, _ ...survey.AskOpt) error {
+				r := response.(*string)
+				*r = test.promptResponse
+
+				if test.shouldErr {
+					return errors.New("error")
+				}
+				return nil
+			})
+
+			port, err := portForwardResource(ioutil.Discard, "image-name")
+			t.CheckErrorAndDeepEqual(test.shouldErr, err, test.expected, port)
+		})
+	}
+}
+
 func TestConfirmInitOptions(t *testing.T) {
 	tests := []struct {
 		description    string
