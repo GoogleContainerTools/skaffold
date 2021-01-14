aaefe8b12b3ef7dc6c2aa507d4328563206a1d3a
gsquared94 <gaghosh@google.com>
2021-01-13 23:25:14 -0800

    Implement container sync for docker deployer.

---
+++ integration/examples/react-reload-docker/app/src/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelloWorld } from './components/HelloWorld.js';
+
+ReactDOM.render( < HelloWorld/>, document.getElementById( 'root' ) );
