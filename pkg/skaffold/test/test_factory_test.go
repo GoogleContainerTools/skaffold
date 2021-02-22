83a1dd72f4d3cb739cbbc802b25e5b17e40e782d
Priya Modali <modali@google.com>
2021-02-21 19:44:15 -0800

    Revert "Moving structure test's dependency file extraction logic to NewTester() to reduce code redundancy."
    This reverts commit 5e1d8591eb4f48a73dab9d0544da269acd1ce830.

--- pkg/skaffold/test/test_factory_test.go
+++ pkg/skaffold/test/test_factory_test.go
@@ -81,18 +81,17 @@ func TestWrongPattern(t *testing.T) {
 			}},
 		}
 
-		_, err := NewTester(cfg, func(imageName string) (bool, error) { return true, nil })
-		// t.CheckNoError(err)
+		tester, err := NewTester(cfg, func(imageName string) (bool, error) { return true, nil })
+		t.CheckNoError(err)
+
+		_, err = tester.TestDependencies()
 		t.CheckError(true, err)
 
-		// _, err = tester.TestDependencies()
-		// t.CheckError(true, err)
-
-		// err = tester.Test(context.Background(), ioutil.Discard, []build.Artifact{{
-		// 	ImageName: "image",
-		// 	Tag:       "image:tag",
-		// }})
-		// t.CheckError(true, err)
+		err = tester.Test(context.Background(), ioutil.Discard, []build.Artifact{{
+			ImageName: "image",
+			Tag:       "image:tag",
+		}})
+		t.CheckError(true, err)
 	})
 }
 
