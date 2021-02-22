b604945b27313d96788d7bd86e4715fe63e07085
ricardo-larosa <ricardo@larosa.dev>
2021-02-22 09:27:47 +0000

    Add darwin/arm64 golint support for M1 macs

--- hack/install_golint.sh
+++ hack/install_golint.sh
@@ -64,6 +64,7 @@ is_supported_platform() {
   found=1
   case "$platform" in
     darwin/amd64) found=0 ;;
+    darwin/arm64) found=0 ;;
     darwin/386) found=0 ;;
     windows/amd64) found=0 ;;
     windows/386) found=0 ;;
