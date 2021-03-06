4e0a0920ee8dcad940e99a96dfb1a82585177309
Priya Modali <modali@google.com>
2021-03-05 18:52:22 -0800

    Disabling artifact caching for custom builder.

--- integration/run_test.go
+++ integration/run_test.go
@@ -108,6 +108,7 @@ func TestRun(t *testing.T) {
 		{
 			description: "custom builder",
 			dir:         "examples/custom",
+			args:        []string{"-cache-artifacts=false"},
 			pods:        []string{"getting-started"},
 		},
 		{
