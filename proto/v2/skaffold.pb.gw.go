0039b131cbee06ae2612bb753fcc43b6a14613c9
Marlon Gamez <marlongamez98@gmail.com>
2021-03-16 16:06:27 -0700

    give v2 proto different package

--- proto/v2/skaffold.pb.gw.go
+++ proto/v2/skaffold.pb.gw.go
@@ -2,11 +2,11 @@
 // source: v2/skaffold.proto
 
 /*
-Package proto is a reverse proxy.
+Package v2 is a reverse proxy.
 
 It translates gRPC into RESTful JSON APIs.
 */
-package proto
+package v2
 
 import (
 	"context"
