c4d7f30e87617105b3e0225cdb17442a6c87ee28
Marlon Gamez <marlongamez98@gmail.com>
2021-04-29 15:11:03 -0700

    add functions for deploy subtasks

---
+++ pkg/skaffold/event/v2/deploy.go
@@ -0,0 +1,43 @@
+package v2
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/constants"
+	sErrors "github.com/GoogleContainerTools/skaffold/pkg/skaffold/errors"
+	proto "github.com/GoogleContainerTools/skaffold/proto/v2"
+)
+
+func DeployInProgress(id int, artifact string) {
+	handler.handleDeploySubtaskEvent(&proto.DeploySubtaskEvent{
+		Id:       strconv.Itoa(id),
+		TaskId:   fmt.Sprintf("%s-%d", constants.Deploy, handler.iteration),
+		Status:   InProgress,
+	})
+}
+
+func DeployFailed(id int, artifact string, err error) {
+	handler.handleDeploySubtaskEvent(&proto.DeploySubtaskEvent{
+		Id:            strconv.Itoa(id),
+		TaskId:        fmt.Sprintf("%s-%d", constants.Deploy, handler.iteration),
+		Status:        Failed,
+		ActionableErr: sErrors.ActionableErrV2(handler.cfg, constants.Deploy, err),
+	})
+}
+
+func DeploySucceeded(id int, artifact string) {
+	handler.handleDeploySubtaskEvent(&proto.DeploySubtaskEvent{
+		Id:       strconv.Itoa(id),
+		TaskId:   fmt.Sprintf("%s-%d", constants.Deploy, handler.iteration),
+		Status:   Succeeded,
+	})
+}
+
+func (ev *eventHandler) handleDeploySubtaskEvent(e *proto.DeploySubtaskEvent) {
+	ev.handle(&proto.Event{
+		EventType: &proto.Event_DeploySubtaskEvent{
+			DeploySubtaskEvent: e,
+		},
+	})
+}
