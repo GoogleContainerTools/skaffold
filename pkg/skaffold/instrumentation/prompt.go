3a64738e0d8f2c5ebbcc8e75f40c9821442f22ba
tejal29 <tejal29@gmail.com>
2021-04-30 13:25:21 -0700

    lint

--- pkg/skaffold/instrumentation/prompt.go
+++ pkg/skaffold/instrumentation/prompt.go
@@ -19,9 +19,10 @@ package instrumentation
 import (
 	"io"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/color"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/config"
-	"github.com/sirupsen/logrus"
 )
 
 const Prompt = `To help improve the quality of this product, we collect anonymized usage data for details on what is tracked and how we use this data visit <https://skaffold.dev/docs/resources/telemetry/>. This data is handled in accordance with our privacy policy <https://policies.google.com/privacy>
