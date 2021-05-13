8117c5752761f151ac2a1414f867ca2c9a48814f
Marlon Gamez <marlongamez98@gmail.com>
2021-05-13 12:00:06 -0700

    emit applicationLogEvents on event endpoint (#5829)
    * emit applicationLogEvents on event endpoint
    
    * add unit test for func

---
+++ pkg/skaffold/event/v2/application_logs.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2021 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v2
+
+import proto "github.com/GoogleContainerTools/skaffold/proto/v2"
+
+func ApplicationLog(podName, containerName, message string) {
+	handler.handleApplicationLogEvent(&proto.ApplicationLogEvent{
+		ContainerName: podName,
+		PodName:       containerName,
+		Message:       message,
+	})
+}
+
+func (ev *eventHandler) handleApplicationLogEvent(e *proto.ApplicationLogEvent) {
+	ev.handle(&proto.Event{
+		EventType: &proto.Event_ApplicationLogEvent{
+			ApplicationLogEvent: e,
+		},
+	})
+}
