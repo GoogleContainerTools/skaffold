ed7a3f3b9053bdd01ce2d2868f30677b7748606f
Marlon Gamez <marlongamez98@gmail.com>
2021-06-10 12:08:33 -0700

    properly set output for deploy

--- pkg/skaffold/runner/v1/deploy.go
+++ pkg/skaffold/runner/v1/deploy.go
@@ -75,7 +75,7 @@ func (r *SkaffoldRunner) Deploy(ctx context.Context, out io.Writer, artifacts []
 		return r.Render(ctx, out, artifacts, false, r.runCtx.RenderOutput())
 	}
 
-	output.WithEventContext(out, constants.Deploy, "-1", "skaffold")
+	out = output.WithEventContext(out, constants.Deploy, "-1", "skaffold")
 
 	output.Default.Fprintln(out, "Tags used in deployment:")
 
