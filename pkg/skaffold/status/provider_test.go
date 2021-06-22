7d48f7e6997368a33a0b6dc5f4f6aec5a847b765
gsquared94 <gaghosh@google.com>
2021-06-22 18:18:29 +0530

    add UT

---
+++ pkg/skaffold/status/provider_test.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2021 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package status
+
+import (
+	"reflect"
+	"sync"
+	"testing"
+
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/config"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/kubernetes/status"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
+	"github.com/GoogleContainerTools/skaffold/testutil"
+)
+
+func TestMonitorProvider(t *testing.T) {
+	tests := []struct {
+		description string
+		statusCheck *bool
+		isNoop      bool
+	}{
+		{
+			description: "unspecified statusCheck parameter",
+		},
+		{
+			description: "statusCheck parameter set to true",
+			statusCheck: util.BoolPtr(true),
+		},
+		{
+			description: "statusCheck parameter set to false",
+			statusCheck: util.BoolPtr(false),
+			isNoop:      true,
+		},
+	}
+
+	for _, test := range tests {
+		once = sync.Once{} // reset once for tests
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			m := NewMonitorProvider(mockConfig{statusCheck: test.statusCheck}, nil).GetKubernetesMonitor()
+			t.CheckDeepEqual(test.isNoop, reflect.Indirect(reflect.ValueOf(m)).Type() == reflect.TypeOf(NoopMonitor{}))
+		})
+	}
+}
+
+type mockConfig struct {
+	status.Config
+	statusCheck *bool
+}
+
+func (m mockConfig) StatusCheck() (*bool, error) { return m.statusCheck, nil }
+
+func (m mockConfig) StatusCheckDeadlineSeconds() int { return 0 }
+
+func (m mockConfig) Muted() config.Muted { return config.Muted{} }
