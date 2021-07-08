a034b3e3e3c2c5eecc201e3ea3c232a4a9bb424d
Nick Kubala <nkubala@google.com>
2021-07-08 12:46:44 -0700

    always log output from k3d image import

--- pkg/skaffold/kubernetes/loader/load.go
+++ pkg/skaffold/kubernetes/loader/load.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -152,10 +153,13 @@ func (i *ImageLoader) loadImages(ctx context.Context, out io.Writer, artifacts [
 		}
 
 		cmd := createCmd(artifact.Tag)
-		if cmdOut, err := util.RunCmdOut(cmd); err != nil {
+		cmdOut, err := util.RunCmdOut(cmd)
+		if err != nil {
 			output.Red.Fprintln(out, "Failed")
 			return fmt.Errorf("unable to load image %q into cluster: %w, %s", artifact.Tag, err, cmdOut)
 		}
+
+		fmt.Fprintf(os.Stdout, "logs from successful load: %s\n", string(cmdOut))
 
 		output.Green.Fprintln(out, "Loaded")
 	}
