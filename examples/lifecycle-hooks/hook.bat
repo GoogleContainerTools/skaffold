6f64e5bb554916c35f2fda71ecec1b535f44805d
Aaron Prindle <aprindle@google.com>
2021-07-30 14:54:30 -0700

    release v1.29.0 (#6333)

---
+++ examples/lifecycle-hooks/hook.bat
@@ -0,0 +1,12 @@
+@echo off 
+
+echo Build specification:
+echo    DefaultRepo:    %SKAFFOLD_DEFAULT_REPO%
+echo    RPCPort:        %SKAFFOLD_RPC_PORT%
+echo    HTTPPort:       %SKAFFOLD_HTTP_PORT%
+echo    WorkDir:        %SKAFFOLD_WORK_DIR%
+echo    Image:          %SKAFFOLD_IMAGE%
+echo    PushImage:      %SKAFFOLD_PUSH_IMAGE%
+echo    ImageRepo:      %SKAFFOLD_IMAGE_REPO%
+echo    ImageTag:       %SKAFFOLD_IMAGE_TAG%
+echo    BuildContext:   %SKAFFOLD_BUILD_CONTEXT%
