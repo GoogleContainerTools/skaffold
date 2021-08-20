c675a7575d9909bba445eac694e101cc008b0f21
Mike Verbanic <verbanicm@users.noreply.github.com>
2021-08-20 12:37:29 -0400

    Fix #6408 - Add `--load-images` flag to deploy (#6428)
    * Initial revision
    
    * Updated docs

--- pkg/skaffold/runner/runcontext/context.go
+++ pkg/skaffold/runner/runcontext/context.go
@@ -173,6 +173,7 @@ func (rc *RunContext) GetKubeNamespace() string                      { return rc
 func (rc *RunContext) GlobalConfig() string                          { return rc.Opts.GlobalConfig }
 func (rc *RunContext) HydratedManifests() []string                   { return rc.Opts.HydratedManifests }
 func (rc *RunContext) LoadImages() bool                              { return rc.Cluster.LoadImages }
+func (rc *RunContext) ForceLoadImages() bool                         { return rc.Opts.ForceLoadImages }
 func (rc *RunContext) MinikubeProfile() string                       { return rc.Opts.MinikubeProfile }
 func (rc *RunContext) Muted() config.Muted                           { return rc.Opts.Muted }
 func (rc *RunContext) NoPruneChildren() bool                         { return rc.Opts.NoPruneChildren }
