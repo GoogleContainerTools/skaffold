7aff46a39859a806193deba7d6036525b8c33024
Glenn Pratt <glenn.pratt@acquia.com>
2021-08-26 14:05:27 -0700

    fix: custom.GetDependencies should execute Command in workspace (#6321)
    * fix: custom.GetDependencies should execute Command in workspace
    
    * add test
    
    * kick travis
    
    * Fix helm_test for newer version.
    
    * bump golangci timeout because it's slow and travis is slow.

--- testutil/cmd_helper.go
+++ testutil/cmd_helper.go
@@ -36,6 +36,7 @@ type run struct {
 	input      []byte
 	output     []byte
 	env        []string
+	dir        *string
 	err        error
 	pipeOutput bool
 }
@@ -79,6 +80,10 @@ func CmdRunErr(command string, err error) *FakeCmd {
 
 func CmdRunOut(command string, output string) *FakeCmd {
 	return newFakeCmd().AndRunOut(command, output)
+}
+
+func CmdRunDirOut(command string, dir string, output string) *FakeCmd {
+	return newFakeCmd().AndRunDirOut(command, dir, output)
 }
 
 func CmdRunOutErr(command string, output string, err error) *FakeCmd {
@@ -140,6 +145,14 @@ func (c *FakeCmd) AndRunOut(command string, output string) *FakeCmd {
 	})
 }
 
+func (c *FakeCmd) AndRunDirOut(command string, dir string, output string) *FakeCmd {
+	return c.addRun(run{
+		command: command,
+		dir:     &dir,
+		output:  []byte(output),
+	})
+}
+
 func (c *FakeCmd) AndRunOutErr(command string, output string, err error) *FakeCmd {
 	return c.addRun(run{
 		command: command,
@@ -169,6 +182,7 @@ func (c *FakeCmd) RunCmdOut(ctx context.Context, cmd *exec.Cmd) ([]byte, error) 
 	}
 
 	c.assertCmdEnv(r.env, cmd.Env)
+	c.assertCmdDir(r.dir, cmd.Dir)
 
 	if err := c.assertInput(cmd, r, command); err != nil {
 		return nil, err
@@ -251,6 +265,18 @@ func (c *FakeCmd) assertCmdEnv(requiredEnv, actualEnv []string) {
 		if _, ok := envs[e]; !ok {
 			c.t.Errorf("expected env variable with value %q", e)
 		}
+	}
+}
+
+// assertCmdDir ensures that actualDir contains matches requiredDir
+func (c *FakeCmd) assertCmdDir(requiredDir *string, actualDir string) {
+	if requiredDir == nil {
+		return
+	}
+	c.t.Helper()
+
+	if *requiredDir != actualDir {
+		c.t.Errorf("expected: %s. Got: %s", *requiredDir, actualDir)
 	}
 }
 
