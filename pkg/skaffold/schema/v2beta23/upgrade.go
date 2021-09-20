29df3733683c957680384f6f2528a3f1776ec627
Ahmet Alp Balkan <ahmetb@google.com>
2021-09-20 10:51:25 -0700

    introduce schema v2beta24
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- pkg/skaffold/schema/v2beta23/upgrade.go
+++ pkg/skaffold/schema/v2beta23/upgrade.go
@@ -14,16 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v2beta22
+package v2beta23
 
 import (
+	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest/v1"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/util"
-	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v2beta23"
 	pkgutil "github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
 )
 
 // Upgrade upgrades a configuration to the next version.
-// Config changes from v2beta22 to v2beta23
+// Config changes from v2beta23 to v2beta24
 func (c *SkaffoldConfig) Upgrade() (util.VersionedConfig, error) {
 	var newConfig next.SkaffoldConfig
 	pkgutil.CloneThroughJSON(c, &newConfig)
@@ -34,26 +34,5 @@ func (c *SkaffoldConfig) Upgrade() (util.VersionedConfig, error) {
 }
 
 func upgradeOnePipeline(oldPipeline, newPipeline interface{}) error {
-	oldBuild := &oldPipeline.(*Pipeline).Build
-	newBuild := &newPipeline.(*next.Pipeline).Build
-
-	// move: docker.Secret
-	//   to: docker.Secrets[]
-	for i, newArtifact := range newBuild.Artifacts {
-		oldArtifact := oldBuild.Artifacts[i]
-
-		docker := oldArtifact.DockerArtifact
-		if docker == nil {
-			continue
-		}
-
-		secret := docker.Secret
-		if secret == nil {
-			continue
-		}
-
-		newArtifact.DockerArtifact.Secrets = []*next.DockerSecret{{ID: secret.ID, Source: secret.Source}}
-	}
-
 	return nil
 }
