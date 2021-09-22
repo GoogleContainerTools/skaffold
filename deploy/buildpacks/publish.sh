6ee26200970421de7c4875642b4cbfaa1e4569a6
Brian de Alwis <bdealwis@google.com>
2021-09-22 15:14:19 -0400

    Update pack image to v0.21.1

--- deploy/buildpacks/publish.sh
+++ deploy/buildpacks/publish.sh
@@ -16,7 +16,7 @@
 
 set -e
 
-PACK_VERSION=v0.18.1
+PACK_VERSION=v0.21.1
 
 docker build . --build-arg PACK_VERSION=${PACK_VERSION} -t gcr.io/k8s-skaffold/pack:${PACK_VERSION} -t gcr.io/k8s-skaffold/pack:latest
 docker push gcr.io/k8s-skaffold/pack:${PACK_VERSION}
