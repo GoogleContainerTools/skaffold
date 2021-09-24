bc0d7af8994fbe35d83257085619f61a1a5cb004
Nick Kubala <nkubala@google.com>
2021-09-24 12:27:15 -0700

    fix sync merge

--- pkg/skaffold/sync/types.go
+++ pkg/skaffold/sync/types.go
@@ -21,14 +21,14 @@ import (
 	"io"
 
 	pkgkubectl "github.com/GoogleContainerTools/skaffold/pkg/skaffold/kubectl"
-	v1 "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest/v1"
+	v2 "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest/v2"
 )
 
 type syncMap map[string][]string
 
 type Item struct {
 	Image    string
-	Artifact *v1.Artifact
+	Artifact *v2.Artifact
 	Copy     map[string][]string
 	Delete   map[string][]string
 }
