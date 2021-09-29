3df44e33ba2724423fa74080effe6c6fd81e12f5
Halvard Skogsrud <halvard@google.com>
2021-09-30 08:52:06 +1000

    fix: Wait for context cancel in k8s pod watcher (#6643)
    Make the Kubernetes pod watcher context aware, and select on
    `context.Done()`. This means we can stop waiting, and acting on, pod
    events when the context has been cancelled.
    
    Remove waiting on `context.Done()` in the Kubernetes log aggregator,
    container manager, and pod port forwarder. This is to eliminate the
    chance that the pod watcher sends a `PodEvent` on a channel without a
    waiting receiver.
    
    This should help avoid deadlocks that can occur when pod watcher event
    receivers stop reading from the channel that they've registered with the
    pod watcher.
    
    We still close the channels on the receiver side, which could increase
    the chances of regression and re-occurrence of this issue.
    
    Also uses a RWMutex in the pod watcher, though we could move this change
    to a separate commit.
    
    Fixes: #6424

--- pkg/skaffold/kubernetes/watcher.go
+++ pkg/skaffold/kubernetes/watcher.go
@@ -33,14 +33,14 @@ import (
 type PodWatcher interface {
 	Register(receiver chan<- PodEvent)
 	Deregister(receiver chan<- PodEvent)
-	Start(kubeContext string, ns []string) (func(), error)
+	Start(ctx context.Context, kubeContext string, ns []string) (func(), error)
 }
 
 // podWatcher is a pod watcher for multiple namespaces.
 type podWatcher struct {
 	podSelector  PodSelector
 	receivers    map[chan<- PodEvent]bool
-	receiverLock sync.Mutex
+	receiverLock sync.RWMutex
 }
 
 type PodEvent struct {
@@ -67,7 +67,7 @@ func (w *podWatcher) Deregister(receiver chan<- PodEvent) {
 	w.receiverLock.Unlock()
 }
 
-func (w *podWatcher) Start(kubeContext string, namespaces []string) (func(), error) {
+func (w *podWatcher) Start(ctx context.Context, kubeContext string, namespaces []string) (func(), error) {
 	if len(w.receivers) == 0 {
 		return func() {}, errors.New("no receiver was registered")
 	}
@@ -97,64 +97,79 @@ func (w *podWatcher) Start(kubeContext string, namespaces []string) (func(), err
 
 		watchers = append(watchers, watcher)
 		go func() {
-			l := log.Entry(context.TODO())
-			for evt := range watcher.ResultChan() {
-				// If the event's type is "ERROR", log and continue.
-				if evt.Type == watch.Error {
-					// These errors sem to arise from the watch stream being closed from a ^C.
-					// evt.Object seems likely to be a https://pkg.go.dev/k8s.io/apimachinery/pkg/apis/meta/v1#Status
-					//    Status{
-					//        Status:Failure,
-					//        Code:500,
-					//        Reason:InternalError,
-					//        Message:an error on the server ("unable to decode an event from the watch stream: http2: response body closed") has prevented the request from succeeding,
-					//        Details:&StatusDetails{
-					//          Causes:[]StatusCause{
-					//            {Type:UnexpectedServerResponse,Message:unable to decode an event from the watch stream: http2: response body closed},
-					//            {Type:ClientWatchDecoding,Message:unable to decode an event from the watch stream: http2: response body closed}},
-					//          RetryAfterSeconds:0}}
-					l.Debugf("podWatcher: got unexpected event of type %s: %v", evt.Type, evt.Object)
-					continue
-				}
-
-				// Grab the pod from the event.
-				pod, ok := evt.Object.(*v1.Pod)
-				if !ok {
-					continue
-				}
-
-				if !w.podSelector.Select(pod) {
-					continue
-				}
-
-				if log.IsTraceLevelEnabled() {
-					st := fmt.Sprintf("podWatcher[%s/%s:%v] phase:%v ", pod.Namespace, pod.Name, evt.Type, pod.Status.Phase)
-					if len(pod.Status.Reason) > 0 {
-						st += fmt.Sprintf("reason:%s ", pod.Status.Reason)
+			l := log.Entry(ctx)
+			defer l.Tracef("podWatcher: cease waiting")
+			l.Tracef("podWatcher: waiting")
+			for {
+				select {
+				case <-ctx.Done():
+					l.Tracef("podWatcher: context canceled, returning")
+					return
+				case evt, ok := <-watcher.ResultChan():
+					if !ok {
+						l.Tracef("podWatcher: channel closed, returning")
+						return
 					}
-					for _, c := range append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...) {
-						switch {
-						case c.State.Waiting != nil:
-							st += fmt.Sprintf("%s<waiting> ", c.Name)
-						case c.State.Running != nil:
-							st += fmt.Sprintf("%s<running> ", c.Name)
-						case c.State.Terminated != nil:
-							st += fmt.Sprintf("%s<terminated> ", c.Name)
+					// If the event's type is "ERROR", log and continue.
+					if evt.Type == watch.Error {
+						// These errors sem to arise from the watch stream being closed from a ^C.
+						// evt.Object seems likely to be a https://pkg.go.dev/k8s.io/apimachinery/pkg/apis/meta/v1#Status
+						//    Status{
+						//        Status:Failure,
+						//        Code:500,
+						//        Reason:InternalError,
+						//        Message:an error on the server ("unable to decode an event from the watch stream: http2: response body closed") has prevented the request from succeeding,
+						//        Details:&StatusDetails{
+						//          Causes:[]StatusCause{
+						//            {Type:UnexpectedServerResponse,Message:unable to decode an event from the watch stream: http2: response body closed},
+						//            {Type:ClientWatchDecoding,Message:unable to decode an event from the watch stream: http2: response body closed}},
+						//          RetryAfterSeconds:0}}
+						l.Debugf("podWatcher: got unexpected event of type %s: %v", evt.Type, evt.Object)
+						continue
+					}
+
+					// Grab the pod from the event.
+					pod, ok := evt.Object.(*v1.Pod)
+					if !ok {
+						continue
+					}
+
+					if !w.podSelector.Select(pod) {
+						continue
+					}
+
+					if log.IsTraceLevelEnabled() {
+						st := fmt.Sprintf("podWatcher[%s/%s:%v] phase:%v ", pod.Namespace, pod.Name, evt.Type, pod.Status.Phase)
+						if len(pod.Status.Reason) > 0 {
+							st += fmt.Sprintf("reason:%s ", pod.Status.Reason)
+						}
+						for _, c := range append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...) {
+							switch {
+							case c.State.Waiting != nil:
+								st += fmt.Sprintf("%s<waiting> ", c.Name)
+							case c.State.Running != nil:
+								st += fmt.Sprintf("%s<running> ", c.Name)
+							case c.State.Terminated != nil:
+								st += fmt.Sprintf("%s<terminated> ", c.Name)
+							}
+						}
+						l.Trace(st)
+					}
+
+					l.Tracef("podWatcher: sending to all receivers")
+					w.receiverLock.RLock()
+					for receiver, open := range w.receivers {
+						if open {
+							l.Tracef("podWatcher: sending event type %v pod name %v namespace %v", evt.Type, pod.GetName(), pod.GetNamespace())
+							receiver <- PodEvent{
+								Type: evt.Type,
+								Pod:  pod,
+							}
 						}
 					}
-					l.Trace(st)
+					w.receiverLock.RUnlock()
+					l.Tracef("podWatcher: done sending to all receivers")
 				}
-
-				w.receiverLock.Lock()
-				for receiver, open := range w.receivers {
-					if open {
-						receiver <- PodEvent{
-							Type: evt.Type,
-							Pod:  pod,
-						}
-					}
-				}
-				w.receiverLock.Unlock()
 			}
 		}()
 	}
