189a55291c18ac850277134d2b8f3eaa2c4f7a1d
Marlon Gamez <marlongamez98@gmail.com>
2022-02-09 16:55:37 -0800

    release: v1.36.0 (#7093)

--- examples/custom-buildx/buildx.sh
+++ examples/custom-buildx/buildx.sh
@@ -9,18 +9,17 @@
 #  (2) update the corresponding node-affinities in k8s/pod.yaml.
 
 # The platforms to build.
-platforms="linux/amd64,linux/arm64" 
 
 # `buildx` uses named _builder_ instances configured for specific platforms.
 # This script creates a `skaffold-builder` as required.
 if ! docker buildx inspect skaffold-builder >/dev/null 2>&1; then
-  docker buildx create --name skaffold-builder --platform $platforms
+  docker buildx create --name skaffold-builder --platform $PLATFORMS
 fi
 
 # Building for multiple platforms requires pushing to a registry
 # as the Docker Daemon cannot load multi-platform images. 
 if [ "$PUSH_IMAGE" = true ]; then
-  args="--platform $platforms --push"
+  args="--platform $PLATFORMS --push"
 else
   args="--load"
 fi
