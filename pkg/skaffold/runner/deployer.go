a35b47ce43b7bde74bdb5322934e1d0a8b327367
Aaron Prindle <aprindle@google.com>
2022-03-02 15:57:37 -0800

    feat: add better support and messaging around using helm with skaffold apply (#7149)

--- pkg/skaffold/runner/deployer.go
+++ pkg/skaffold/runner/deployer.go
@@ -66,11 +66,43 @@ func (d *deployerCtx) JSONParseConfig() v1.JSONParseConfig {
 
 // GetDeployer creates a deployer from a given RunContext and deploy pipeline definitions.
 func GetDeployer(ctx context.Context, runCtx *runcontext.RunContext, labeller *label.DefaultLabeller) (deploy.Deployer, error) {
+	deployerCfg := runCtx.Deployers()
+
 	if runCtx.Opts.Apply {
+		helmNamespaces := make(map[string]bool)
+		nonHelmDeployFound := false
+
+		for _, d := range deployerCfg {
+			if d.DockerDeploy != nil || d.KptDeploy != nil || d.KubectlDeploy != nil || d.KustomizeDeploy != nil {
+				nonHelmDeployFound = true
+			}
+
+			if d.HelmDeploy != nil {
+				for _, release := range d.HelmDeploy.Releases {
+					if release.Namespace != "" {
+						helmNamespaces[release.Namespace] = true
+					}
+				}
+			}
+		}
+
+		if len(helmNamespaces) > 1 || (nonHelmDeployFound && len(helmNamespaces) == 1) {
+			return nil, errors.New("skaffold apply called with conflicting namespaces set via skaffold.yaml. This is likely due to the use of the 'deploy.helm.releases.*.namespace' field which is not supported in apply.  Remove the 'deploy.helm.releases.*.namespace' field(s) and run skaffold apply again")
+		}
+
+		if len(helmNamespaces) == 1 && !nonHelmDeployFound {
+			if runCtx.Opts.Namespace == "" {
+				// if skaffold --namespace flag not set, use the helm namespace value
+				for k := range helmNamespaces {
+					// map only has 1 (k,v) from length check in if condition
+					runCtx.Opts.Namespace = k
+				}
+			}
+		}
+
 		return getDefaultDeployer(runCtx, labeller)
 	}
 
-	deployerCfg := runCtx.Deployers()
 	localDeploy := false
 	remoteDeploy := false
 
