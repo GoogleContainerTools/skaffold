603060ea33d142b5d511d67593c31c3906c1022e
Thomas Mota <tmm@danskecommodities.com>
2022-04-12 09:57:19 +0200

    v2beta28 upgrade.go

---
+++ pkg/skaffold/schema/v2beta28/upgrade.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2020 The Skaffold Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v2beta28
+
+import (
+	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest/v1"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/util"
+	pkgutil "github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
+)
+
+// Upgrade upgrades a configuration to the next version.
+// Config changes from v2beta28 to v2beta29
+func (c *SkaffoldConfig) Upgrade() (util.VersionedConfig, error) {
+	var newConfig next.SkaffoldConfig
+	pkgutil.CloneThroughJSON(c, &newConfig)
+	newConfig.APIVersion = next.Version
+
+	err := util.UpgradePipelines(c, &newConfig, upgradeOnePipeline)
+	return &newConfig, err
+}
+
+func upgradeOnePipeline(oldPipeline, newPipeline interface{}) error {
+	oldBuild := &oldPipeline.(*Pipeline).Build
+	newBuild := &newPipeline.(*next.Pipeline).Build
+
+	// transform: artifact.BuildpackArtifact.*[]BuildpackVolume
+	// into		: artifact.BuildpackArtifact.[]*BuildpackVolume
+	for i, newArtifact := range newBuild.Artifacts {
+		oldArtifact := oldBuild.Artifacts[i]
+		if oldArtifact.BuildpackArtifact == nil || len(*oldArtifact.BuildpackArtifact.Volumes) == 0 {
+			continue
+		}
+		for _, volume := range *oldArtifact.BuildpackArtifact.Volumes {
+			newVolume := &next.BuildpackVolume{
+				Host:    volume.Host,
+				Target:  volume.Target,
+				Options: volume.Options,
+			}
+			newArtifact.BuildpackArtifact.Volumes = append(newArtifact.BuildpackArtifact.Volumes, newVolume)
+		}
+	}
+
+	return nil
+}
