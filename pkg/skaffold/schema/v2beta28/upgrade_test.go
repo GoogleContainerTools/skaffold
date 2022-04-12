237fd341828d04d8c4b320128581a3e18d483a9f
Thomas Mota <tmm@danskecommodities.com>
2022-04-12 12:53:35 +0200

    fix formatting

--- pkg/skaffold/schema/v2beta28/upgrade_test.go
+++ pkg/skaffold/schema/v2beta28/upgrade_test.go
@@ -14,18 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v2beta27
+package v2beta28
 
 import (
 	"testing"
 
-	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v2beta28"
+	next "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/latest/v1"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/yaml"
 	"github.com/GoogleContainerTools/skaffold/testutil"
 )
 
 func TestUpgrade(t *testing.T) {
-	yaml := `apiVersion: skaffold/v2beta27
+	yaml := `apiVersion: skaffold/v2beta28
 kind: Config
 build:
   artifacts:
@@ -51,8 +51,8 @@ build:
     sync:
       auto: true
   - image: ko://github.com/GoogleContainerTools/skaffold/cmd/skaffold
-    ko:
-      platforms: ['linux/arm64', 'linux/amd64']
+    ko: {}
+    platforms: ['linux/arm64', 'linux/amd64']
   googleCloudBuild:
     projectId: test-project
 test:
@@ -107,7 +107,8 @@ profiles:
         - k8s-*
       kustomize: {}
 `
-	expected := `apiVersion: skaffold/v2beta28
+
+	expected := `apiVersion: skaffold/v2beta29
 kind: Config
 build:
   artifacts:
