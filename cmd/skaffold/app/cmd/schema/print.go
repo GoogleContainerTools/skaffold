19e03021961aaa7edb00a63d0592c63376e979a2
ericwork <ericwork@google.com>
2022-04-26 09:55:37 -0700

    fix: print cannot export schema and add license for fs

--- cmd/skaffold/app/cmd/schema/print.go
+++ cmd/skaffold/app/cmd/schema/print.go
@@ -28,7 +28,7 @@ import (
 // Print prints the json schema for a given version.
 func Print(out io.Writer, version string) error {
 
-	filename := path.Join("/assets/schemas_generated", strings.TrimPrefix(version, "skaffold/")+".json")
+	filename := path.Join("assets/schemas_generated", strings.TrimPrefix(version, "skaffold/")+".json")
 
 	content, err := fs.AssetsFS.ReadFile(filename)
 	if err != nil {
