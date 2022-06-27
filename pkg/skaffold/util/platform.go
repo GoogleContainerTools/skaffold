0531dfa43801a7b1de1f7eb746b711e017a296fa
Gaurav <39389231+gsquared94@users.noreply.github.com>
2022-06-28 01:24:06 +0530

    fix: index out of range error (#7593)

--- pkg/skaffold/util/platform.go
+++ pkg/skaffold/util/platform.go
@@ -21,6 +21,6 @@ import (
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-func ConvertToV1Platform(platform specs.Platform) *v1.Platform {
-	return &v1.Platform{Architecture: platform.Architecture, OS: platform.OS, OSVersion: platform.OSVersion, OSFeatures: platform.OSFeatures, Variant: platform.Variant}
+func ConvertToV1Platform(platform specs.Platform) v1.Platform {
+	return v1.Platform{Architecture: platform.Architecture, OS: platform.OS, OSVersion: platform.OSVersion, OSFeatures: platform.OSFeatures, Variant: platform.Variant}
 }
