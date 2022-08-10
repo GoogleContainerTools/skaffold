f6c1e1e0e1de94a60b06a95bc4858d2ac5ed55a8
Gaurav Ghosh <gaghosh@google.com>
2022-08-11 01:32:59 +0530

    fix: render should be online for `dev`, `deploy` and `run`.

--- pkg/skaffold/runner/v2/dev.go
+++ pkg/skaffold/runner/v2/dev.go
@@ -348,7 +348,7 @@ func (r *SkaffoldRunner) Dev(ctx context.Context, out io.Writer, artifacts []*la
 	r.deployer.GetLogger().SetSince(time.Now())
 
 	// First render
-	manifests, err := r.Render(ctx, out, r.Builds, true)
+	manifests, err := r.Render(ctx, out, r.Builds, false)
 	r.deployManifests = manifests
 	if err != nil {
 		event.DevLoopFailedInPhase(r.devIteration, constants.Render, err)
