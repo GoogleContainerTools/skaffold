ee9a7d73218ce96eadfe249486f9263fe5de9c60
Gaurav <39389231+gsquared94@users.noreply.github.com>
2022-08-16 01:31:41 +0530

    fix: render should be online for `dev`, `deploy` and `run`. (#7768)

--- cmd/skaffold/app/cmd/deploy.go
+++ cmd/skaffold/app/cmd/deploy.go
@@ -52,7 +52,7 @@ func doDeploy(ctx context.Context, out io.Writer) error {
 			return err
 		}
 		// Render
-		manifests, errR := r.Render(ctx, out, buildArtifacts, true)
+		manifests, errR := r.Render(ctx, out, buildArtifacts, false)
 		if errR != nil {
 			return errR
 		}
