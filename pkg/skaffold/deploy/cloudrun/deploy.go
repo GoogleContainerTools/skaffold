d5b8bd1ce09babbbb1e25431148c2b43b6bcfe8f
Benjamin Kaplan <bkaplan@outlook.com>
2022-09-30 21:51:33 +0000

    test: adding unit tests for deploying Cloud Run Jobs

--- pkg/skaffold/deploy/cloudrun/deploy.go
+++ pkg/skaffold/deploy/cloudrun/deploy.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -291,7 +291,7 @@ func (d *Deployer) deployJob(crclient *run.APIService, manifest []byte, out io.W
 		Job:     job.Metadata.Name,
 	}
 	output.Default.Fprintln(out, "Deploying Cloud Run service:\n\t", job.Metadata.Name)
-	parent := fmt.Sprintf("projects/%s/locations/%s", job.Metadata.Namespace, d.Region)
+	parent := fmt.Sprintf("namespaces/%s", job.Metadata.Namespace)
 
 	sName := resName.String()
 	getCall := crclient.Namespaces.Jobs.Get(sName)
--- pkg/skaffold/deploy/cloudrun/deploy_test.go
+++ pkg/skaffold/deploy/cloudrun/deploy_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,7 +40,7 @@ import (
 	"github.com/GoogleContainerTools/skaffold/testutil"
 )
 
-func TestDeploy(tOuter *testing.T) {
+func TestDeployService(tOuter *testing.T) {
 	tests := []struct {
 		description    string
 		toDeploy       *run.Service
@@ -98,6 +98,122 @@ func TestDeploy(tOuter *testing.T) {
 			toDeploy: &run.Service{
 				ApiVersion: "serving.knative.dev/v1",
 				Kind:       "Service",
+				Metadata: &run.ObjectMeta{
+					Name: "test-service",
+				},
+			},
+			errCode: proto.StatusCode_DEPLOY_READ_MANIFEST_ERR,
+		},
+	}
+	for _, test := range tests {
+		testutil.Run(tOuter, test.description, func(t *testutil.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if test.httpErr != 0 {
+					http.Error(w, "test expecting error", test.httpErr)
+					return
+				}
+				if r.URL.Path != test.expectedPath {
+					http.Error(w, "unexpected path: "+r.URL.Path, http.StatusNotFound)
+				}
+				var service run.Service
+				body, err := io.ReadAll(r.Body)
+				if err != nil {
+					http.Error(w, "Unable to read body: "+err.Error(), http.StatusInternalServerError)
+					return
+				}
+				if err = json.Unmarshal(body, &service); err != nil {
+					http.Error(w, "Unable to parse service: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				b, err := json.Marshal(service)
+				if err != nil {
+					http.Error(w, "unable to marshal response: "+err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Write(b)
+			}))
+
+			configName := "default"
+			deployer, _ := NewDeployer(&runcontext.RunContext{}, &label.DefaultLabeller{}, &latest.CloudRunDeploy{ProjectID: test.defaultProject, Region: test.region}, configName)
+			deployer.clientOptions = append(deployer.clientOptions, option.WithEndpoint(ts.URL), option.WithoutAuthentication())
+			deployer.useGcpOptions = false
+			manifestList, _ := json.Marshal(test.toDeploy)
+			manifestsByConfig := manifest.NewManifestListByConfig()
+			manifestsByConfig.Add(configName, manifest.ManifestList{manifestList})
+			err := deployer.Deploy(context.Background(), os.Stderr, []graph.Artifact{}, manifestsByConfig)
+			if test.errCode == proto.StatusCode_OK && err != nil {
+				t.Fatalf("Expected success but got err: %v", err)
+			} else if test.errCode != proto.StatusCode_OK {
+				if err == nil {
+					t.Fatalf("Expected status code %s but got success", test.errCode)
+				}
+				sErr := err.(sErrors.Error)
+				if sErr.StatusCode() != test.errCode {
+					t.Fatalf("Expected status code %v but got %v", test.errCode, sErr.StatusCode())
+				}
+			}
+		})
+	}
+}
+
+func TestDeployJob(tOuter *testing.T) {
+	tests := []struct {
+		description    string
+		toDeploy       *run.Job
+		defaultProject string
+		region         string
+		expectedPath   string
+		httpErr        int
+		errCode        proto.StatusCode
+	}{
+		{
+			description:    "test deploy",
+			defaultProject: "testProject",
+			region:         "us-central1",
+			expectedPath:   "/apis/run.googleapis.com/v1/namespaces/testProject/jobs",
+			toDeploy: &run.Job{
+				ApiVersion: "run.googleapis.com/v1",
+				Kind:       "Job",
+				Metadata: &run.ObjectMeta{
+					Name: "test-service",
+				},
+			},
+		},
+		{
+			description:    "test deploy with specified project",
+			defaultProject: "testProject",
+			region:         "us-central1",
+			expectedPath:   "/apis/run.googleapis.com/v1/namespaces/testProject/jobs",
+			toDeploy: &run.Job{
+				ApiVersion: "run.googleapis.com/v1",
+				Kind:       "Job",
+				Metadata: &run.ObjectMeta{
+					Name:      "test-service",
+					Namespace: "my-project",
+				},
+			},
+		},
+		{
+			description:    "test permission denied on deploy errors",
+			defaultProject: "testProject",
+			region:         "us-central1",
+			httpErr:        http.StatusUnauthorized,
+			toDeploy: &run.Job{
+				ApiVersion: "run.googleapis.com/v1",
+				Kind:       "Job",
+				Metadata: &run.ObjectMeta{
+					Name:      "test-service",
+					Namespace: "my-project",
+				},
+			},
+			errCode: proto.StatusCode_DEPLOY_CLOUD_RUN_GET_SERVICE_ERR,
+		},
+		{
+			description: "test no project specified",
+			region:      "us-central1",
+			toDeploy: &run.Job{
+				ApiVersion: "run.googleapis.com/v1",
+				Kind:       "Job",
 				Metadata: &run.ObjectMeta{
 					Name: "test-service",
 				},
