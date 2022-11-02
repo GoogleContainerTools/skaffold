cfa7ac79f429d80dbfa5d8972f70fe97c3ae43d8
Aaron Prindle <aprindle@google.com>
2022-11-02 18:02:43 +0000

    chore: bump skaffold schema version to v3beta1

--- pkg/skaffold/schema/versions.go
+++ pkg/skaffold/schema/versions.go
@@ -88,6 +88,7 @@ import (
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v2beta7"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v2beta8"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v2beta9"
+	v3 "github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v3"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/schema/v3alpha1"
 	misc "github.com/GoogleContainerTools/skaffold/pkg/skaffold/util"
 )
@@ -162,6 +163,7 @@ var SchemaVersionsV1 = Versions{
 	{v2beta28.Version, v2beta28.NewSkaffoldConfig},
 	{latestV1.Version, latestV1.NewSkaffoldConfig},
 	{v3alpha1.Version, v3alpha1.NewSkaffoldConfig},
+	{v3.Version, v3.NewSkaffoldConfig},
 }
 
 // SchemaVersionsV2 refers to all the supported API Schemas for skaffold v2 executables. The API schema versions are
