dc964d00ea9123f62f022b3c9704bab79733cb3b
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-12-21 18:32:39 +0100

    feat: more meaningful error message for kustomize issues: #8218 (#8220)
    * feat: more meaningful error message for kustomize issues: #8218
    
    a kustomize error is now surfaced correctly pinpointing to the
    actual file yielding error.
    
    * removed emoji and errorid. Plus downcased first word
    
    as per other error messages Ive just looked at.

--- pkg/skaffold/render/generate/util.go
+++ pkg/skaffold/render/generate/util.go
@@ -100,7 +100,7 @@ func DependenciesForKustomization(dir string) ([]string, error) {
 
 	content := kustomization{}
 	if err := yaml.Unmarshal(buf, &content); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("kustomization parse error in %v: %w", path, err)
 	}
 
 	deps = append(deps, path)
