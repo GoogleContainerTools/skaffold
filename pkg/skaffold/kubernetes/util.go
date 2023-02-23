320d4b32dbebc9715c2b54cf39e32002c4a3f771
Aaron Prindle <aprindle@google.com>
2023-02-23 08:42:56 +0000

    fix: output cause of kubernetes manifest parsing error vs generic message

--- pkg/skaffold/kubernetes/util.go
+++ pkg/skaffold/kubernetes/util.go
@@ -55,6 +55,9 @@ func HasKubernetesFileExtension(n string) bool {
 // IsKubernetesManifest is for determining if a file is a valid Kubernetes manifest
 func IsKubernetesManifest(file string) bool {
 	_, err := parseKubernetesObjects(file)
+	if err != nil {
+		log.Entry(context.TODO()).Error(err)
+	}
 	return err == nil
 }
 
