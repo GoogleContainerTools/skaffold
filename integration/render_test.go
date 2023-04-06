d325564b9d0e7f0604ad45bd17b5314381a90f28
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-04-06 07:45:46 -0400

    chore: nit

--- integration/render_test.go
+++ integration/render_test.go
@@ -1197,7 +1197,7 @@ manifests:
     paths:
     - overlays/dev
 `, input: map[string]string{
-			"base/kustomization.yaml": `apiVersion: kustomize.config.k8s.io/v1beta1
+				"base/kustomization.yaml": `apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 
 resources:
