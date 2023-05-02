f8d89faabebcc1697c54da77eaf5ac02e5976769
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-05-02 16:19:31 -0400

    chore: update licenses

--- fs/assets/credits_generated/github.com/letsencrypt/boulder/link.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Symlink the various boulder subcommands into place.
-#
-BINDIR="$PWD/bin"
-for n in `"${BINDIR}/boulder" --list` ; do
-  ln -sf boulder "${BINDIR}/$n"
-done
--- fs/assets/credits_generated/github.com/letsencrypt/boulder/test/grpc-creds/README.txt
+++
@@ -1,1 +0,0 @@
-See ../test/PKI.md
