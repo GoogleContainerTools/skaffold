edb40adb1105490781b6cc5a36f43f9b2c7f5f1e
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-05-04 13:27:29 -0400

    fix: scanning filter not working properly due to version sorting (#8727)

--- deploy/vuln-monitor/scan.sh
+++ deploy/vuln-monitor/scan.sh
@@ -44,7 +44,7 @@ check_vulnerability(){
     done
     tags_filter+="^edge$"
     # get the latest patches tags for lts images. gcloud will return extra tags if an image has multiple tags and we only want tags specified in the filter, so use grep to further filter the result.
-    tags=$(gcloud container images list-tags "$base_image" --filter="tags~$tags_filter" --format='value(tags)' | sort -nr | awk -F'[.]' '$1$2!=p&&p=$1$2' | grep -Po "$tags_filter|edge")
+    tags=$(gcloud container images list-tags "$base_image" --filter="tags~$tags_filter" --format='value(tags)' | sort -Vr | awk -F'[.]' '$1$2!=p&&p=$1$2' | grep -Po "$tags_filter|edge")
   fi
 
   for tagsByComma in $tags; do
