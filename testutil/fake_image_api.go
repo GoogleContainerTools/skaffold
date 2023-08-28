4a458a1cf0a3181897d79900b4733324cdd19e65
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-08-28 11:20:28 -0400

    fix: bazel test by creating starstand fake tar

--- testutil/fake_image_api.go
+++ testutil/fake_image_api.go
@@ -21,9 +21,11 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/random"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
 	"io"
 	"math"
-	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -309,14 +311,24 @@ func (f *FakeAPIClient) Close() error { return nil }
 
 // TODO(dgageot): create something that looks more like an actual tar file.
 func CreateFakeImageTar(ref string, path string) error {
-	return os.WriteFile(path, []byte(ref), os.ModePerm)
+	image, err := random.Image(1024, 1)
+	if err != nil {
+		return fmt.Errorf("failed to create fake image %w", err)
+	}
+	reference, err := name.ParseReference(ref)
+	if err != nil {
+		return fmt.Errorf("failed to parse reference %w", err)
+	}
+	return tarball.WriteToFile(path, reference, image)
 }
 
 func ReadRefFromFakeTar(input io.Reader) (string, error) {
-	buf, err := io.ReadAll(input)
+	manifest, err := tarball.LoadManifest(func() (io.ReadCloser, error) {
+		return io.NopCloser(input), nil
+	})
 	if err != nil {
-		return "", fmt.Errorf("reading tar")
+		return "", fmt.Errorf("loading manifest %w", err)
 	}
 
-	return string(buf), nil
+	return manifest[0].RepoTags[0], nil
 }
