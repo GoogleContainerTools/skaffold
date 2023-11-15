7a655ba9ffa59d98e5af5be29025d4da4ec82fb0
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-11-15 14:00:36 -0500

    chore: upgrade docker, minikube version,comments nit

--- pkg/skaffold/build/docker/docker.go
+++ pkg/skaffold/build/docker/docker.go
@@ -71,7 +71,6 @@ func (b *Builder) Build(ctx context.Context, out io.Writer, a *latest.Artifact, 
 
 	// ignore useCLI boolean if buildkit is enabled since buildkit is only implemented for docker CLI at the moment in skaffold.
 	// we might consider a different approach in the future.
-	// use CLI for cross-platform builds
 	if b.useCLI || (b.useBuildKit != nil && *b.useBuildKit) || len(a.DockerArtifact.CliFlags) > 0 {
 		imageID, err = b.dockerCLIBuild(ctx, output.GetUnderlyingWriter(out), a.ImageName, a.Workspace, dockerfile, a.ArtifactType.DockerArtifact, opts)
 	} else {
