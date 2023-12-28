2ee41d418a9b17d8aad5f39ab8721f45e0854266
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-12-28 13:56:58 -0500

    chore: test

--- pkg/skaffold/hooks/render_test.go
+++ pkg/skaffold/hooks/render_test.go
@@ -80,7 +80,7 @@ func TestRenderHooksLinux(t *testing.T) {
 					{
 						HostHook: &latest.HostHook{
 							OS:      []string{"windows"},
-							Command: []string{"pwsh", "-Command", "echo pre-hook running with SKAFFOLD_KUBE_CONTEXT=%SKAFFOLD_KUBE_CONTEXT%,SKAFFOLD_NAMESPACES=%SKAFFOLD_NAMESPACES%"},
+							Command: []string{"pwsh", "-Command", "echo", "pre-hook running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"},
 						},
 					},
 				},
@@ -88,7 +88,7 @@ func TestRenderHooksLinux(t *testing.T) {
 					{
 						HostHook: &latest.PostRenderHostHook{
 							OS:      []string{"windows"},
-							Command: []string{"pwsh", "-Command", "echo post-hook running with SKAFFOLD_KUBE_CONTEXT=%SKAFFOLD_KUBE_CONTEXT%,SKAFFOLD_NAMESPACES=%SKAFFOLD_NAMESPACES%"},
+							Command: []string{"pwsh", "-Command", "echo", "post-hook running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"},
 						},
 					},
 				},
@@ -170,7 +170,7 @@ func TestRenderHooksWindows(t *testing.T) {
 					{
 						HostHook: &latest.HostHook{
 							OS:      []string{"windows"},
-							Command: []string{"pwsh", "-Command", "echo", "running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"},
+							Command: []string{"pwsh", "-Command", `echo "pre-hook running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"`},
 						},
 					},
 				},
@@ -178,7 +178,7 @@ func TestRenderHooksWindows(t *testing.T) {
 					{
 						HostHook: &latest.PostRenderHostHook{
 							OS:      []string{"windows"},
-							Command: []string{"pwsh", "-Command", "echo", "running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"},
+							Command: []string{"pwsh", "-Command", `echo "post-hook running with SKAFFOLD_KUBE_CONTEXT=$([Environment]::GetEnvironmentVariable('SKAFFOLD_KUBE_CONTEXT')),SKAFFOLD_NAMESPACES=$([Environment]::GetEnvironmentVariable('SKAFFOLD_NAMESPACES'))"`},
 						},
 					},
 				},
@@ -220,7 +220,7 @@ func TestRenderHooksWindows(t *testing.T) {
 			t.CheckContains(test.preHostHookOut, string(preOut))
 			_, err = runner.RunPostHooks(context.Background(), manifest.ManifestList{}, os.Stdout)
 			t.CheckNoError(err)
-			postOut, err := os.ReadFile(preOutFile)
+			postOut, err := os.ReadFile(postOutFile)
 			t.CheckError(test.shouldErr, err)
 			t.CheckContains(test.postHostHookOut, string(postOut))
 		})
