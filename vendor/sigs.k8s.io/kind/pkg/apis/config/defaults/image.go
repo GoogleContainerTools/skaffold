a211ba31667200bb6c0619b2d894b86374b4fb53
Michael Plump <plumpy@google.com>
2024-12-19 00:46:00 -0500

    chore: go mod vendor

--- vendor/sigs.k8s.io/kind/pkg/apis/config/defaults/image.go
+++ vendor/sigs.k8s.io/kind/pkg/apis/config/defaults/image.go
@@ -18,4 +18,4 @@ limitations under the License.
 package defaults
 
 // Image is the default for the Config.Image field, aka the default node image.
-const Image = "kindest/node:v1.31.2@sha256:18fbefc20a7113353c7b75b5c869d7145a6abd6269154825872dc59c1329912e"
+const Image = "kindest/node:v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027"
