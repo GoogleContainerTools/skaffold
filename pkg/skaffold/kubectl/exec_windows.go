e3024cb84ccdc00b0a73d3e1970af47f912eb349
SeongChan Lee <foriequal@gmail.com>
2025-02-21 17:53:22 +0900

    use `reflect` to make it future proof(?)

--- pkg/skaffold/kubectl/exec_windows.go
+++ pkg/skaffold/kubectl/exec_windows.go
@@ -19,9 +19,9 @@ package kubectl
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
-	"sync"
-	"sync/atomic"
+	"reflect"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -64,15 +64,7 @@ func (c *Cmd) Start() error {
 		return fmt.Errorf("could not start the command: %w", err)
 	}
 
-	// Use `unsafe` to extract the process handle.
-	processHandle := (*struct {
-		Pid    int
-		mode   uint8 // processMode
-		state  atomic.Uint64
-		sigMu  sync.RWMutex
-		handle windows.Handle
-	})(unsafe.Pointer(c.Process)).handle
-
+	processHandle := getHandleFromProcess(c.Process)
 	if err := windows.AssignProcessToJobObject(handle, processHandle); err != nil {
 		return fmt.Errorf("could not assign job object: %w", err)
 	}
@@ -84,6 +76,13 @@ func (c *Cmd) Start() error {
 	}()
 
 	return nil
+}
+
+func getHandleFromProcess(p *os.Process) windows.Handle {
+	// `os.Process` don't expose `handle uintptr` field.
+	v := reflect.ValueOf(p)
+	f := reflect.Indirect(v).FieldByName("handle")
+	return windows.Handle(f.Uint())
 }
 
 // Run starts the specified command in a job object and waits for it to complete
