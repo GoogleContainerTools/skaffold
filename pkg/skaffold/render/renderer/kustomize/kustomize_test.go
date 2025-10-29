0f3f942b72379084e42f8272e32661f371f69edb
Joan Coll Cerdán <johan.az@gmail.com>
2025-10-29 18:15:27 +0100

    chore(kustomize): Added more tests related to configmap and secret generators
    Added tests that validate support of env files and regular files aliased with
    keys when using kustomize during the render stage, and when running
    skaffold in dev mode.

--- pkg/skaffold/render/renderer/kustomize/kustomize_test.go
+++ pkg/skaffold/render/renderer/kustomize/kustomize_test.go
@@ -17,8 +17,11 @@ limitations under the License.
 package kustomize
 
 import (
+	"slices"
 	"testing"
 
+	"github.com/GoogleContainerTools/skaffold/v2/pkg/skaffold/render"
+	"github.com/GoogleContainerTools/skaffold/v2/pkg/skaffold/schema/latest"
 	"github.com/GoogleContainerTools/skaffold/v2/testutil"
 )
 
@@ -83,6 +86,108 @@ func TestBuildCommandArgs(t *testing.T) {
 		testutil.Run(t, test.description, func(t *testutil.T) {
 			args := kustomizeBuildArgs(test.buildArgs, test.kustomizePath)
 			t.CheckDeepEqual(test.expectedArgs, args)
+		})
+	}
+}
+
+func TestMirror(t *testing.T) {
+	tests := []struct {
+		description string
+		shouldErr   bool
+		createFiles map[string]string
+		touchFiles  []string
+	}{
+		{
+			description: "mirror configmap and secret generators using env files, regular files, and regular files with keys",
+			shouldErr:   false,
+			createFiles: map[string]string{
+				"kustomization.yaml": `configMapGenerator:
+  - name: app-env
+    envs:
+      - configmaps/app-env/app.env
+  - name: app-config
+    files:
+      - credentials.pub=configmaps/app-config/credentials.local.pub
+      - configmaps/app-config/setup.json
+
+secretGenerator:
+  - name: app-env-secrets
+    envs:
+      - secrets/app-env-secrets/secrets.env
+  - name: app-config-secrets
+    files:
+      - credentials.key=secrets/app-config-secrets/credentials.local.key
+      - secrets/app-config-secrets/eyesonly.txt
+`},
+			touchFiles: []string{
+				"configmaps/app-env/app.env",
+				"configmaps/app-config/credentials.local.pub",
+				"configmaps/app-config/setup.json",
+				"secrets/app-env-secrets/secrets.env",
+				"secrets/app-config-secrets/credentials.local.key",
+				"secrets/app-config-secrets/eyesonly.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		testutil.Run(t, test.description, func(t *testutil.T) {
+			// Create the file structure required for the test
+			sourceDir := t.NewTempDir()
+
+			for path, contents := range test.createFiles {
+				sourceDir.Write(path, contents)
+			}
+
+			sourceDir.Touch(test.touchFiles...)
+
+			// Create the instance to test
+			mockCfg := render.MockConfig{
+				WorkingDir: sourceDir.Root(),
+			}
+
+			rc := latest.RenderConfig{
+				Generate: latest.Generate{
+					Kustomize: &latest.Kustomize{
+						Paths: []string{sourceDir.Root()},
+					},
+				},
+			}
+
+			k, err := New(mockCfg, rc, map[string]string{}, "default", "", nil, false)
+			t.CheckNoError(err)
+
+			// Test the mirror function on a temporary workspace
+			targetDir := t.NewTempDir()
+			fs := newTmpFS(targetDir.Root())
+			defer fs.Cleanup()
+
+			t_err := k.mirror(sourceDir.Root(), fs)
+
+			// Gather a list of the expected files
+			expectedFiles, err := sourceDir.List()
+			t.CheckNoError(err)
+
+			slices.Sort(expectedFiles)
+
+			// Gather a list of files that have been created by the mirror function
+			targetFileList, err := targetDir.List()
+			t.CheckNoError(err)
+
+			minPrefixLen := len(targetDir.Root()) + len(sourceDir.Root())
+			prefixLen := len(targetDir.Root())
+
+			var mirroredFiles []string
+			for _, f := range targetFileList {
+				if len(f) >= minPrefixLen {
+					mirroredFiles = append(mirroredFiles, f[prefixLen:])
+				}
+			}
+
+			slices.Sort(mirroredFiles)
+
+			// Validate test results
+			t.CheckErrorAndDeepEqual(test.shouldErr, t_err, expectedFiles, mirroredFiles)
 		})
 	}
 }
