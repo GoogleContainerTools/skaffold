592f4aad31ddf010e80fcc05f4a701f00ee82463
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2025-12-17 16:22:18 +0100

    remoteManifests: respect skaffold namespace
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- pkg/skaffold/render/generate/generate_test.go
+++ pkg/skaffold/render/generate/generate_test.go
@@ -103,7 +103,7 @@ func TestGenerate(t *testing.T) {
 				Touch("empty.ignored").
 				Chdir()
 
-			g := NewGenerator(".", test.generateConfig, "")
+			g := NewGenerator(".", test.generateConfig, "", "")
 			var output bytes.Buffer
 			actual, err := g.Generate(context.Background(), &output)
 			t.CheckNoError(err)
@@ -121,7 +121,7 @@ func TestGenerateFromURLManifest(t *testing.T) {
 	defer os.RemoveAll(manifest.ManifestTmpDir)
 	g := NewGenerator(".", latest.Generate{
 		RawK8s: []string{ts.URL},
-	}, "")
+	}, "", "")
 	var output bytes.Buffer
 	actual, err := g.Generate(context.Background(), &output)
 	testutil.Run(t, "", func(t *testutil.T) {
